7aa473cd7a16cc5551df5b337cd92ecc90dcc28b
Niels Renard <code.i.pushed@gmail.com>
2018-12-02 14:06:27 +0100

    d2 **

--- src/adventofcode_2018/day_02.clj
+++ src/adventofcode_2018/day_02.clj
@@ -9,6 +9,7 @@
         threes (get-duplicates puz-in 3)]
     (* twos threes)))
 
+
 (defn get-duplicates
   "returns how many duplicate n's there are in a collection"
   [col n] (->> col
@@ -18,3 +19,29 @@
                (map #(filter #{n} %))
                (filter not-empty)
                count))
+
+
+(defn solve-part-2
+  [puz-in]
+  (let [answer (for [s1 puz-in
+                     s2 puz-in]
+                 (just-one-difference? s1 s2))]
+    (->> answer
+         (remove nil?)
+         first
+         (remove nil?)
+         (apply str))))
+
+
+(defn just-one-difference?
+  "Returns the diff for strings with one character difference."
+  [s1 s2]
+  (let [diff   (clojure.data/diff (seq s1) (seq s2))
+        length (count s1)]
+    (if (->> diff
+             last
+             (remove nil? ,,,)
+             count
+             (- length ,,,)
+             (= 1 ,,,))
+      (last diff))))
