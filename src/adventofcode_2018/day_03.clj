d473c41b526f519a9698f75aa1d536ab7fb8119e
Niels Renard <place@holder.com>
2018-12-04 00:46:20 +0100

    d3 * OOF

--- src/adventofcode_2018/day_03.clj
+++ src/adventofcode_2018/day_03.clj
@@ -1,8 +1,97 @@
 (ns adventofcode-2018.day-03
-  (:require [util :refer :all]))
+  (:require [util :refer :all]
+            [clojure.edn :as edn]
+            [clojure.string :as string]))
 
 (def input (util/get-input))
 
+;;committing the mess before cleanup to enjoy in git in the future
+
+(defn sanitize-entry
+  "parses an line like:
+  `#1 @ 483,830: 24x18`
+  into an entry `((483 830) (26 16))`."
+  [line]
+  (partition 2
+             (drop 1
+                   (map #(edn/read-string %) ;;parse to ints
+                        (-> line
+                            (string/replace ":" "")
+                            (string/replace "#" "")
+                            (string/replace "@ " "")
+                            (string/replace "," " ")
+                            (string/replace "x" " ")
+                            (string/split #" "))))))
+
+;;this is a piece of cloth
+;;use (get-in @grid-atom [0 1]) to get square 0,1
+(def grid-atom (atom [[[0,0] [0,1] [0,2]]
+                      [[1,0] [1,1] [1,2]]
+                      [[2,0] [2,1] [2,2]]]))
+
+(def fabric (atom {}))
+
+;;(def fabric (atom {0 { 0 1 1 2 2 3 }
+;;                   1 { 0 2 1 3 2 5 }
+;;                   2 { 0 5 1 3 2 2 }
+;;                   3 { 0 3 1 2 2 4 }}))
+
+;;((1 1) (2 2)) should return
+;;{(1 1), (1 2), (2 1), (2 2)}
+
+(defn entry->surface
+  "takes a sanitized entry and returns all coordinates"
+  [entry]
+  (let [x (ffirst entry)
+        y (second (first entry))
+        p (first (second entry))
+        q (second (second entry))]
+    (for [x' (range x (if (zero? x) (+ x p) (+ x p)))
+          y' (range y (if (zero? y) (+ y q) (+ y q)))]
+      [x' y'])))
+
+(defn surface-painter
+  [[x y]]
+  (let [sqinch (get-in @fabric [x y])]
+    (if (nil? sqinch)
+      (swap! fabric assoc-in [x y] 0)
+      (swap! fabric assoc-in [x y] (inc sqinch)))))
+
+(comment (map #(map surface-painter %) (->> input
+                                            (map sanitize-entry)
+                                            (map entry->surface)
+                                            )))
+
+;;107000 too low
+;;too high Please wait one minute before trying again. (You guessed 158971.)
+(count (distinct (reduce concat all-painted)))
+
+(defn paint
+  [line]
+  (let [x      (ffirst line)
+        y      (second (first line))
+        db     @fabric
+        sqinch (get-in db [x y])]
+    (if (nil? sqinch)
+      (swap! fabric assoc-in [x y] 0)
+      (swap! fabric assoc-in [x y] (inc sqinch)))))
+
+
 (defn solve-part-1
-  [puz-in] -1
-  )
+  [puz-in]
+  (->> puz-in
+       (map sanitize-entry)
+       (map entry->surface) ;;return all claimed coordinates [[x y] [[x y] [x y]]]
+       (reduce concat)      ;;flatten list [[x y][x y][x y]]
+       frequencies          ;;count [[x y] 3]
+       vals                 ;;             [3]
+       (filter #(< 1 %))    ;;             [3]
+       count))              ;;             [1]
+
+(defn get-all-claimed
+  "returns every claimed square inch"
+  [puz-in]
+  (->> puz-in
+       (map sanitize-entry)
+       (map entry->surface)
+       (reduce concat)))
--- test/adventofcode_2018/day_03_test.clj
+++ test/adventofcode_2018/day_03_test.clj
@@ -5,7 +5,7 @@
 
 (deftest solve-part-1
   ;;The four square inches marked with X are claimed
-  ;;by both 1 and 2. (Clim 3, while adjacent to the
+  ;;by both 1 and 2. (Claim 3, while adjacent to the
   ;;others, does not overlap either of them.)
   ;; Visually, these claim the following areas:
   ;; ........
@@ -20,3 +20,79 @@
          (sut/solve-part-1 ["#1 @ 1,3: 4x4"
                             "#2 @ 3,1: 4x4"
                             "#3 @ 5,5: 2x2"]))))
+
+;; ........
+;; ...2222.
+;; ...2222.
+;; .11XX22.
+;; .11XYX2.
+;; .111X3..
+;; .1111...
+;; ........
+(deftest solve-part-1-three-things-overlap
+  (is (= 6
+         (sut/solve-part-1 ["#1 @ 1,3: 4x4"
+                            "#2 @ 3,1: 4x4"
+                            "#3 @ 4,4: 2x2"]))))
+
+;; Y.......
+;; 5.......
+;; X.......
+;; ........
+;; ........
+;; ........
+;; ........
+;; ........
+(deftest solve-part-1-four-things-overlap
+  (is (= 2
+         (sut/solve-part-1 ["#1 @ 0,0 1x1"
+                            "#2 @ 0,0 1x1"
+                            "#3 @ 0,0 1x1"
+                            "#4 @ 0,0 1x1"
+                            "#5 @ 0,0 1x3"
+                            "#6 @ 0,2 1x1"]))))
+
+
+
+;; 1111....
+;; 111X222.
+;; 111X222.
+;; 111X222.
+;; ...2222.
+;; .....33.
+;; .....33.
+;; ........
+(deftest solve-part-1-with-zeroes
+  (is (= 3
+         (sut/solve-part-1 ["#1 @ 0,0: 4x4"
+                            "#2 @ 3,1: 4x4"
+                            "#3 @ 5,5: 2x2"]))))
+
+;; 11111...
+;; 111XX22.
+;; 111XX22.
+;; 111XX22.
+;; ...2222.
+;; .....33.
+;; .....33.
+;; ........
+(deftest solve-part-1-width-bigger-than-height
+  (is (= 6
+         (sut/solve-part-1 ["#1 @ 0,0: 5x4"
+                            "#2 @ 3,1: 4x4"
+                            "#3 @ 5,5: 2x2"]))))
+
+;; 1111....
+;; 111X222.
+;; 111X222.
+;; 111X222.
+;; 111X222.
+;; .....33.
+;; .....XX4
+;; .....444
+(deftest solve-part-1-height-bigger-than-width
+  (is (= 6
+         (sut/solve-part-1 ["#1 @ 0,0: 4x5"
+                            "#2 @ 3,1: 4x4"
+                            "#3 @ 5,5: 2x2"
+                            "#4 @ 5,6: 20x20"]))))
