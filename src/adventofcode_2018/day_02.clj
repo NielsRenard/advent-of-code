edb39219e84909d7204f0639dca5cbce9dce51fe
Niels Renard <code.i.pushed@gmail.com>
2018-12-03 06:51:56 +0100

    d3 tests

--- src/adventofcode_2018/day_02.clj
+++ src/adventofcode_2018/day_02.clj
@@ -1,13 +1,8 @@
 (ns adventofcode-2018.day-02
-  (:require [util :refer :all]))
+  (:require [util :refer :all]
+            [clojure.data :as data]))
 
 (def input (util/get-input))
-
-(defn solve-part-1
-  [puz-in]
-  (let [twos   (get-duplicates puz-in 2)
-        threes (get-duplicates puz-in 3)]
-    (* twos threes)))
 
 
 (defn get-duplicates
@@ -20,6 +15,27 @@
                (filter not-empty)
                count))
 
+(defn solve-part-1
+  [puz-in]
+  (let [twos   (get-duplicates puz-in 2)
+        threes (get-duplicates puz-in 3)]
+    (* twos threes)))
+
+
+
+;;TODO: optimize by halting the list comprehension when a value is found
+(defn just-one-difference?
+  "Returns the diff for strings with one character difference."
+  [s1 s2]
+  (let [diff   (data/diff (seq s1) (seq s2))
+        length (count s1)]
+    (if (->> diff
+             last
+             (remove nil?)
+             count
+             (- length)
+             (= 1))
+      (last diff))))
 
 (defn solve-part-2
   [puz-in]
@@ -31,17 +47,3 @@
          first
          (remove nil?)
          (apply str))))
-
-
-(defn just-one-difference?
-  "Returns the diff for strings with one character difference."
-  [s1 s2]
-  (let [diff   (clojure.data/diff (seq s1) (seq s2))
-        length (count s1)]
-    (if (->> diff
-             last
-             (remove nil? ,,,)
-             count
-             (- length ,,,)
-             (= 1 ,,,))
-      (last diff))))
--- test/adventofcode_2018/day_03_test.clj
+++ test/adventofcode_2018/day_03_test.clj
@@ -1,3 +1,22 @@
 (ns adventofcode-2018.day-03-test
   (:require [adventofcode-2018.day-03 :as sut]
-            [clojure.test :as t]))
+            [clojure.test :refer [deftest is]]))
+
+
+(deftest solve-part-1
+  ;;The four square inches marked with X are claimed
+  ;;by both 1 and 2. (Clim 3, while adjacent to the
+  ;;others, does not overlap either of them.)
+  ;; Visually, these claim the following areas:
+  ;; ........
+  ;; ...2222.
+  ;; ...2222.
+  ;; .11XX22.
+  ;; .11XX22.
+  ;; .111133.
+  ;; .111133.
+  ;; ........
+  (is (= 4
+         (sut/solve-part-1 ["#1 @ 1,3: 4x4"
+                            "#2 @ 3,1: 4x4"
+                            "#3 @ 5,5: 2x2"]))))
