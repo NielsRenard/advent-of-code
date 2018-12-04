13a993578ebd68c1b2cad8a7887c14981f95e09e
Niels Renard <place@holder.com>
2018-12-04 21:39:13 +0100

    d4 checkin laptop

--- src/adventofcode_2018/day_04.clj
+++ src/adventofcode_2018/day_04.clj
@@ -1,4 +1,18 @@
 (ns adventofcode-2018.day-04
-  (:require [util :refer :all]))
+  (:require [util :refer :all]
+            [clojure.string :as string]
+            [java-time :as t]))
 
 (def input (get-input))
+
+
+(defn parse-date [line]
+  (t/local-date "yyyy-MM-dd" (-> line
+                                 (string/replace "[" "")
+                                 (string/replace "]" "")
+                                 (string/split #" ")
+                                 first)))
+
+(defn solve-part-1
+  [puz-in]
+  -1)
--- test/adventofcode_2018/day_04_test.clj
+++ test/adventofcode_2018/day_04_test.clj
@@ -1,3 +1,46 @@
 (ns adventofcode-2018.day-04-test
   (:require [adventofcode-2018.day-04 :as sut]
-            [clojure.test :as t]))
+            [clojure.test :refer [deftest is]]))
+
+;; Note that guards count as asleep on the minute they fall asleep, and they count
+;; as awake on the minute they wake up. For example, because Guard #10 wakes up at
+;; 00:25 on 1518-11-01, minute 25 is marked as awake.
+
+;; Visually, these records show that the guards are asleep at these times:
+;;
+;; Date   ID   Minute
+;;             000000000011111111112222222222333333333344444444445555555555
+;;             012345678901234567890123456789012345678901234567890123456789
+;; 11-01  #10  .....####################.....#########################.....
+;; 11-02  #99  ........................................##########..........
+;; 11-03  #10  ........................#####...............................
+;; 11-04  #99  ....................................##########..............
+;; 11-05  #99  .............................................##########.....
+
+
+(deftest solve-part-1
+  ;; Strategy 1: Find the guard that has the most minutes asleep. What minute
+  ;; does that guard spend asleep the most?  In the example above, Guard #10 spent
+  ;; the most minutes asleep, a total of 50 minutes (20+25+5), while Guard #99 only
+  ;; slept for a total of 30 minutes (10+10+10). Guard #10 was asleep most during
+  ;; minute 24 (on two days, whereas any other minute the guard was asleep was only
+  ;; seen on one day).
+
+  (is (= 240
+         (sut/solve-part-1 ["[1518-11-01 00:00] Guard #10 begins shift"
+                            "[1518-11-01 00:05] falls asleep"
+                            "[1518-11-01 00:25] wakes up"
+                            "[1518-11-01 00:30] falls asleep"
+                            "[1518-11-01 00:55] wakes up"
+                            "[1518-11-01 23:58] Guard #99 begins shift"
+                            "[1518-11-02 00:40] falls asleep"
+                            "[1518-11-02 00:50] wakes up"
+                            "[1518-11-03 00:05] Guard #10 begins shift"
+                            "[1518-11-03 00:24] falls asleep"
+                            "[1518-11-03 00:29] wakes up"
+                            "[1518-11-04 00:02] Guard #99 begins shift"
+                            "[1518-11-04 00:36] falls asleep"
+                            "[1518-11-04 00:46] wakes up"
+                            "[1518-11-05 00:03] Guard #99 begins shift"
+                            "[1518-11-05 00:45] falls asleep"
+                            "[1518-11-05 00:55] wakes up"]))))
