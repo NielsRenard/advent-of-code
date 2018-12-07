18129a26dbd9287e5b2fc13b2e38a36bff276111
Niels Renard <place@holder.com>
2018-12-07 18:15:37 +0100

    d5.1 * brute-force mutable string solution

--- src/adventofcode_2018/day_05.clj
+++ src/adventofcode_2018/day_05.clj
@@ -1,4 +1,52 @@
 (ns adventofcode-2018.day-05
   (:require [util :refer :all]))
 
-(def input (util/get-input))
+(def input (first (util/get-input)))
+
+(def exin "dabAcCaCBAcCcaDA")
+
+(defn react?
+  "checks if two elements are the same type,
+  and opposite polarity."
+  [[c1 c2]]
+  (= 32 (Math/abs (- (int c1)
+                     (int c2)))))
+
+;;doesn't work
+(comment (defn solve-part-1
+           ([input]
+            (solve-part-1 input 0 ))
+           ([input cursor]
+            (let [char-pair   ((juxt first second) (drop cursor input))
+                  new-cursor  (inc cursor)]
+              (if (and (< new-cursor (count input))
+                       (< new-cursor 5500))
+                (if (react? char-pair)
+                  (recur (clojure.string/replace input (apply str char-pair) "") 0)
+                  (recur input new-cursor))
+                input)))))
+
+
+(def lower (seq "abcdefghijklmnopqrstuvwxyz"))
+(def upper (seq "ABCDEFGHIJKLMNOPQRSTUVWXYZ"))
+(def char-pairs (partition 2 (interleave lower upper upper lower)))
+
+(defn remove-pair-from-string
+  [string pair]
+  (clojure.string/replace string (apply str pair) ""))
+
+;;n n'
+(defn zzz
+  [input cnt]
+  (let [new-count (count (drop 1 input))]
+    (if (= new-count cnt)
+      input
+      (remove-pair-from-string (drop 1 input) new-count))))
+
+
+
+(def mutablestring (atom input))
+
+(def brute-force-answer (dotimes [n 490]
+                          (for [cs char-pairs]
+                            (swap! mutablestring #(remove-pair-from-string % cs)))))
--- test/adventofcode_2018/day_05_test.clj
+++ test/adventofcode_2018/day_05_test.clj
@@ -1,3 +1,34 @@
 (ns adventofcode-2018.day-05-test
   (:require [adventofcode-2018.day-05 :as sut]
-            [clojure.test :as t]))
+            [clojure.test :refer [deftest is are]]))
+
+
+(deftest react?-test
+  (are [charpair expected] (= expected (sut/react? charpair))
+    [\a \A] true
+    [\A \a] true
+    [\a \a] false
+    [\A \A] false
+    [\z \Z] true
+    [\Z \z] true))
+
+(deftest solve-part-1-test
+  (are [input expected] (= expected (sut/solve-part-1 input))
+;;    ;;In aA, a and A react, leaving nothing behind.
+;;    "aA"     ""
+;;    ;;In abBA, bB destroys itself, leaving aA. As above,
+;;    ;;this then destroys itself, leaving nothing.
+;;    "abBA"   ""
+;;    ;;In abAB, no two adjacent units are of the same type,
+;;    ;;and so nothing happens.
+;;    "abAB"   "abAB"
+;;    ;;In aabAAB, even though aa and AA are of the same type, their polarities match, and so nothing happens.
+;;    "aabAAB" "aabAAB"
+;;
+;;    ;;Now, consider a larger example, dabAcCaCBAcCcaDA:
+;;    ;;dabAcCaCBAcCcaDA  The first 'cC' is removed.
+;;    ;;dabAaCBAcCcaDA    This creates 'Aa', which is removed.
+;;    ;;dabCBAcCcaDA      Either 'cC' or 'Cc' are removed (the result is the same).
+;;    ;;dabCBAcaDA        No further actions can be taken.
+;;    "dabAcCaCBAcCcaDA" "dabCBAcaDA"
+    "AabAcaCBAaccDAa" "bAcaCBccD"))
