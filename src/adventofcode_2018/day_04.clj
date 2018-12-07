cfcb4447269118a3cdb6e35970399c62c2302595
Niels Renard <place@holder.com>
2018-12-07 14:13:08 +0100

    refactor day 4
    (time (solve-part-1 input))
    "Elapsed time: 290.288673 msecs"
    (time (solve-part-2 input))
    "Elapsed time: 265.521453 msecs"

--- src/adventofcode_2018/day_04.clj
+++ src/adventofcode_2018/day_04.clj
@@ -6,20 +6,7 @@
 
 (def input (get-input))
 
-
-
 (defn parse-date [line]
-  "parses the date to a java-time local-date object"
-  (let [split (-> line
-                  (string/replace "[" "")
-                  (string/split #"]"))
-        date  (->> split
-                   first
-                   (t/local-date-time "yyyy-MM-dd HH:mm"))
-        event (string/trim (first (rest split)))]
-    [date event]))
-
-(defn new-parse-date [line]
   "parses the date to a java-time local-date object"
   (let [split (-> line
                   (string/replace "[" "")
@@ -40,14 +27,9 @@
 (defn sort-chronologically
   [input]
   (->> input
-       (map new-parse-date)
+       (map parse-date)
        sort))
 
-
-(defn sleep-duration
-  "takes two temporals and returns the duration between them"
-  [falls-asleep wakes-up]
-  (t/duration falls-asleep wakes-up))
 
 (defn sleep-duration-with-starting
   "takes two temporals and returns the duration between them"
@@ -73,18 +55,6 @@
   "Takes a shift, calculates sleep duration."
   [shift]
   (let [start-shift-event (first shift)
-        sleep-wake-events (map first (map reverse (second shift)))]
-    {:who start-shift-event
-     :sleep(->> sleep-wake-events
-                (partition 2)
-                (map #(apply sleep-duration %))
-                (apply t/plus))}))
-
-
-(defn new-time-asleep
-  "Takes a shift, calculates sleep duration."
-  [shift]
-  (let [start-shift-event (first shift)
         sleep-wake-events (map second (map reverse (second shift)))]
     {:who start-shift-event
      :sleep(->> sleep-wake-events
@@ -93,18 +63,8 @@
                 )}))
 
 
+
 (defn split-by-shift
-  "Takes a chronologically sorted seq of events and splits them by shift. "
-  [chrono-sorted-seq]
-  (let [first-shift-elf (first chrono-sorted-seq)]
-    (->> chrono-sorted-seq
-         (map reverse)
-         (partition-by #(number? (first %)))
-         (partition 2))))
-
-(+ 2137)
-
-(defn new-split-by-shift
   "Takes a chronologically sorted seq of events and splits them by shift. "
   [chrono-sorted-seq]
   (let [first-shift-elf (first chrono-sorted-seq)]
@@ -114,29 +74,38 @@
 
 (defn get-all-shifts-of-guard
   "gets all shifts of a certain elf id"
-  [id]
-  (filter #(= id (second (ffirst %))) (-> input
+  [puz-in id]
+  (filter #(= id (second (ffirst %))) (-> puz-in
                                           sort-chronologically
-                                          new-split-by-shift)))
+                                          split-by-shift)))
 
-;;TODO rewrite this so it reads better, and make it a function
-(def all-guard-ids (->>
-                    (-> input
-                        sort-chronologically
-                        new-split-by-shift)
-                    (group-by #(second (ffirst %)))
-                    ((comp sort keys))))
+;;could probably be a lot more effient with a regex
+;;using the split-by-shift output makes this slow
+(defn get-all-guard-ids
+  "returns the ids of all the guards, sorted low to high"
+  [puz-in]
+  (let [sorted-input (-> puz-in
+                         sort-chronologically
+                         split-by-shift)
+        get-id-fn    (fn [line]
+                       (second (ffirst line)))]
+    (->> sorted-input
+         (group-by get-id-fn)
+         keys
+         sort)))
+
 
 (defn get-shift-durations
   [shifts]
   (->> shifts
-       (map new-time-asleep)
+       (map time-asleep)
        (map second)
        (map second)))
 
+
 (defn most-slept-minute
-  "takes a seq of these kind of maps
-  ({:started-at 42, :duration 16}"
+  "calculates all the minutes a guard was asleep, and
+  returns the minute that is most frequent."
   [durations]
   (->> durations
        (map #(range (:started-at %) (+ (:duration %)
@@ -144,79 +113,42 @@
        flatten
        frequencies
        (sort-by val)
-       last
-       first
-       ))
-
-(defn maaa
-  "takes a seq of these kind of maps
-  ({:started-at 42, :duration 16}"
-  [durations]
-  (->> durations
-       (map #(range (:started-at %) (+ (:duration %)
-                                       (:started-at %))))
-       flatten
-       frequencies
-
-       ))
-
-(defn baaa
-  [id] (maaa (flatten (get-shift-durations (get-all-shifts-of-guard id)))))
+       last))
 
 
-(defn find-most-slept-minute-guard
-  [id] (most-slept-minute (flatten (get-shift-durations (get-all-shifts-of-guard id)))))
+(defn find-most-slept-minute-of-guard
+  "returns the minute the guard was most frequently asleep (and how many times this occured)."
+  [puz-in id] (most-slept-minute (flatten (get-shift-durations (get-all-shifts-of-guard puz-in id)))))
 
 
-(defn my-most-slept-minute
-  [durations]
-  (for [s (map #(map :started-at %) durations)
-        d (map #(map :duration %) durations)]
-    [(count s) (count d)]))
-
-(defn total-slept-minutes [id]
-  (reduce + (flatten (map #(map :duration %) (get-shift-durations (get-all-shifts-of-guard id))))))
+(defn total-slept-minutes [puz-in id]
+  "calculates how many minutes this elf slept"
+  (reduce + (flatten (map #(map :duration %) (get-shift-durations (get-all-shifts-of-guard puz-in id))))))
 
 
 (defn solve-part-1
+  "Strategy 1: Find the guard that has the most minutes asleep.
+   What minute does that guard spend asleep the most?"
   [puz-in]
-  (->> puz-in
-       sort-chronologically ;; parse-and-sort
-       new-split-by-shift       ;;
-       (map new-time-asleep)    ;; calc all sleep
-       ))   ;; sort by longest sleep (it's guard 2137)
-                                        ;
+  (let [all-guard-ids      (get-all-guard-ids puz-in)
+        most-sleepy-guard  (->> (map #(total-slept-minutes puz-in %) all-guard-ids)
+                                (#(zipmap % all-guard-ids))
+                                (apply max-key key)
+                                val)
+        most-sleepy-minute (key (find-most-slept-minute-of-guard puz-in most-sleepy-guard))]
+    (* most-sleepy-guard
+       most-sleepy-minute)))
 
+(defn solve-part-2
+  "Strategy 2: Of all guards, which guard
+  is most frequently asleep on the same minute?"
+  [puz-in]
+  (let [all-guard-ids (get-all-guard-ids puz-in)]
 
-(comment
-
-  ;; part two:
-  ;; (map find-most-slept-minute all-guard-ids)
-  ;; check which has the highest frequency of a number
-  ;; poof thats it
-
-  ;;let's do a binary search
-  ;;64110 too low (2137 * 30)
-  ;;96165 too low 37 * 45)
-  ;;81206 too high (2137 * 38)
-  ;;74795 wrong * 35 (no high or low anymore)
-
-  ;;elf 1993 has most minutes asleep but I get multiple hits for most slept minute
-
-  ;;that one elfs shifts
-  (def sleepy-shifts (filter #(= 2137 (first (ffirst %))) (-> input
-                                                              sort-chronologically
-                                                              new-split-by-shift)))
-
-  (def durations
-    (->> sleepy-shifts
-         (map new-time-asleep)
-         (map second)
-         (map second)))
-
-  (def (sort-by val (frequencies (flatten (most-slept-minute (get-shift-durations (get-all-shifts-of-guard 2137))))))
-    (frequencies (flatten (most-slept-minute durations))))
-
-  ;;that one guys total sleep times
-  (filter #(= 2137 ((comp ffirst :who) %)) here
-          ))
+    (->> (map  #(find-most-slept-minute-of-guard puz-in %) all-guard-ids)
+         (zipmap all-guard-ids)
+         (sort-by (comp val val))
+         last
+         flatten
+         drop-last
+         (reduce *))))
