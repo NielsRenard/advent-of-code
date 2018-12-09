0d8896ba24ff1f9d91d6406d58786397bbacb9b7
Niels Renard <place@holder.com>
2018-12-09 19:05:45 +0100

    cleanup d7.1

--- src/adventofcode_2018/day_07.clj
+++ src/adventofcode_2018/day_07.clj
@@ -18,7 +18,6 @@
                   (string/split #" ")
                   (#(map keyword %))
                   )]
-    ;;refactor this :must keyword out of the vector
     {(first split) #{(second split)}}))
 
 (defn parse-data [puz-in]
@@ -36,13 +35,10 @@
                        flatten
                        distinct)]
     (->> instructions
-         (map (comp seq val)) ;; get the unlockable steps [:A [:C :B] [:D :C]]
+         (map (comp seq val))    ;; get the unlockable steps [:A [:C :B] [:D :C]]
          flatten                 ;;               [:A  :C :B   :D :C ]
          (concat first-row)      ;; combine steps with no pre-req
          set                     ;; dedupe       #{:A  :B :C :D}
-         ;;delet this
-         #_(diff alphabet)         ;; diff against alphabet
-         #_last                    ;; take the common keys
          )))
 
 
@@ -60,56 +56,11 @@
          sort
          )))
 
-;; TODO: too much nesting, refactor '(:must (:C) :before [:A :F]
-;; to just {:C [:A :F]}
 (defn get-unlocks
   "takes a step and returns what other steps they unlock."
   [step parsed-data]
   (->> parsed-data
        step))
-
-;; TODO: think of better names
-(defn remove-satisfied-befores
-  "Removes keys from :before column in the parsed data."
-  [keys parsed-data]
-  (->> parsed-data
-       (map #(update % :before (fn [coll] (remove keys coll))))))
-
-
-(defn remove-satisfied-steps
-  "If the before collection is empty, remove the whole entry."
-  [parsed-data]
-  (let [answer (->> parsed-data
-                    (filter (comp not-empty :before)))]
-    answer))
-
-(def example-input
-  ["Step C must be finished before step A can begin."
-   "Step C must be finished before step F can begin."
-   "Step A must be finished before step B can begin."
-   "Step A must be finished before step D can begin."
-   "Step B must be finished before step E can begin."
-   "Step D must be finished before step E can begin."
-   "Step F must be finished before step E can begin."])
-
-
-#_(defn solve-part-1
-    [puz-in]
-    (let [parsed-data (parse-data example-input)]
-      (->>  parsed-data
-            find-possible-steps
-            first
-            (map #(get-unlocks % parsed-data))
-            )))
-
-(def bed (atom #{}))
-
-(defn can-be-removed?
-  [key parsed-data]
-  (->> parsed-data
-       :before
-       #_       flatten
-       #_(some #{key})))
 
 (defn remove-finished-steps
   [finished-steps parsed-data]
@@ -135,11 +86,11 @@
 
 (defn solve-part-1
   ([input]
-   (solve-part-1 nil [:starting input]))    ;; 1 arg self calling fn to kick it off
+   (solve-part-1 nil [:starting input]))          ;; 1 arg self calling fn to kick it off
   ([answer remaining-data]
    (let [step      (first remaining-data)
          remaining (second remaining-data)]
-     (if (nil? step)                        ;; break out of the recur when no next steps
+     (if (nil? step)                              ;; break out of the recur when no next steps
        (->> answer
             reverse
             rest
@@ -149,16 +100,5 @@
 
        (->> remaining
             one-round
-            (recur (conj answer step))      ;; add the step to the answer and run again
+            (recur (conj answer step))            ;; add the step to the answer and run again
             )))))
-
-(comment
-  ;;wrong, didn't take into account mid-step shifts
-  ;;GKPTSXBILJNUCDFMVAHEOWYQR
-  ;;GKPTSLUXBIJMNCADFOVHEWYQR
-  (parse-data-structure input)
-  (parse-data-structure example-input)
-  (map (fn [ul pd] (can-be-removed? ul pd)) new-unlocks)
-  (swap! bed union (first possible-steps) )
-  ;; this does nothing because possible-steps are already out of :before
-  (remove-satisfied-befores (first possible-steps)))
