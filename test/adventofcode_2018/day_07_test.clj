34924df9dac3c037f7f03aa220e77c3caa6d601b
Niels Renard <place@holder.com>
2018-12-09 18:39:59 +0100

    d7.1 * with test pre-cut

--- test/adventofcode_2018/day_07_test.clj
+++ test/adventofcode_2018/day_07_test.clj
@@ -3,8 +3,6 @@
             [clojure.test :refer [is are deftest]]))
 
 
-;; I added the last line because the input did not cover
-;; the case of multiple starting nodes.
 (def example-input
   ["Step C must be finished before step A can begin."
    "Step C must be finished before step F can begin."
@@ -12,9 +10,9 @@
    "Step A must be finished before step D can begin."
    "Step B must be finished before step E can begin."
    "Step D must be finished before step E can begin."
-   "Step F must be finished before step E can begin."
-   "Step Q must be finished before step A can begin."])
+   "Step F must be finished before step E can begin."])
 
 (deftest solve-part-1
-  (is (= [:C :Q :A :B :D :F]
-         (sut/solve-part-1 example-input))))
+  (is (= "CABDFE"
+         (-> (sut/parse-data example-input)
+             (sut/solve-part-1 )))))
