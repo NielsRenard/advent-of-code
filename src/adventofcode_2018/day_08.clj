0036708f59fe4bd0adcd85b8dcdea407f4223868
Niels Renard <place@holder.com>
2018-12-10 00:59:51 +0100

    setup some days resources (add doall to line-seq in util)

--- src/adventofcode_2018/day_08.clj
+++ src/adventofcode_2018/day_08.clj
@@ -1,4 +1,16 @@
 (ns adventofcode-2018.day-08
-  (:require [util :refer :all]))
+  (:require [util :refer :all]
+            [clojure.edn :as edn]
+            [clojure.string :as string]))
 
-(def input (util/get-input))
+(def input (->> (first (util/get-input))
+                split-by-whitespace
+                (map edn/read-string)))
+
+(def example-input
+  '(2 3 0 3 10 11 12 1 1 0 1 99 2 1 1 2))
+
+(defn solve-part-1
+  [puz-in]
+  -1
+  )
--- src/adventofcode_2018/day_12.clj
+++ src/adventofcode_2018/day_12.clj
@@ -2,3 +2,8 @@
   (:require [util :refer :all]))
 
 (def input (util/get-input))
+
+(defn solve-part-1
+  [puz-in]
+  -1
+  )
