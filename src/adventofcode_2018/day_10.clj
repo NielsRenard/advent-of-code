403071d8f0bc5cf74fdd651d98a1e2cf16979a6a
Niels Renard <place@holder.com>
2018-12-11 06:06:24 +0100

    day 10 starting, am session

--- src/adventofcode_2018/day_10.clj
+++ src/adventofcode_2018/day_10.clj
@@ -1,7 +1,42 @@
 (ns adventofcode-2018.day-10
-  (:require [util :refer :all]))
+  (:require [util :refer :all]
+            [clojure.edn :as edn]
+            [clojure.string :as string]))
 
 (def input (util/get-input))
+
+(defn parse-line
+  "parses this string:
+    position=< 9,  1> velocity=< 0,  2>
+    {:x 9, :y 1, :dx 5, :dy 1}"
+  [line]
+  (let [split (-> line
+                  (string/replace "position=<" "")
+                  (string/replace ", " " ")
+                  (string/replace "> velocity=<" " ")
+                  (string/replace ", " " ")
+                  (string/replace ">" "")
+                  (string/split #" ")
+                  (->>
+                   (filter #(not (empty? %)))
+                   (map edn/read-string)))]
+
+    {:x  (first split) :y  (second split)
+     :dx (nth split 2) :dy (last split)}))
+
+(defn translate
+  "Takes a pair of coordinates & and its velocity, calculates
+   the next position. e.g.
+   {:x 9,  :y 1, :dx 5, :dy 1} becomes
+   {:x 14, :y 2, :dx 5, :dy 1}"
+  [{:keys [x y dx dy]}]
+  {:x (+ x dx) :y (+ y dy) :dx dx :dy dy})
+
+
+(defn parse-data [puz-in]
+  (->> puz-in
+       (map parse-line)))
+
 
 (defn solve-part-1
   [puz-in]
--- test/adventofcode_2018/day_10_test.clj
+++ test/adventofcode_2018/day_10_test.clj
@@ -1,3 +1,11 @@
 (ns adventofcode-2018.day-10-test
   (:require [adventofcode-2018.day-10 :as sut]
-            [clojure.test :as t]))
+            [clojure.test :refer [is are deftest]]))
+
+
+(deftest translate-test
+  (are [input expected] (= expected
+                           (sut/translate input))
+    {:x 9  :y 1 :dx 5 :dy  1}  {:x 14 :y 2 :dx 5 :dy 1}
+    {:x 3  :y 9 :dx 1 :dy -2}  {:x 4 :y 7 :dx 1 :dy -2}
+    ))
