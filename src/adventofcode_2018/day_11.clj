0bab68667af33f6945d21a2d5e21c5fc0376a1c6
Niels Renard <code.i.pushed@gmail.com>
2018-12-17 00:34:45 +0100

    day 11.1 * solution + test

--- src/adventofcode_2018/day_11.clj
+++ src/adventofcode_2018/day_11.clj
@@ -4,6 +4,7 @@
 
 (def input 3999)
 
+;;cut this down
 (defn find-power-level
   [[serial x y]]
   (let [rack-id      (+ x 10)
@@ -19,13 +20,87 @@
                          )]
     (- power-level 5)))
 
-(defn grid-maker
-  [serial]
-  (for [x (range 300)
-        y (range 300)]
-    (find-power-level [serial x y])))
+(defn get-power-levels
+  [serial size]
+  (for [x (range 0 size)
+        y (range 0 size)]
+    {:x x :y y :pl (find-power-level [serial x y])}))
+
+
+(defn- blank-canvas [size]
+  (vec (repeat size (vec (repeat size "_")))))
+
+(defn- put-one-in-grid [canvas {:keys [x y pl]}]
+  (assoc-in canvas [x y] pl))
+
+
+;;not necessary but fun to print grids
+(defn print-grid [canvas]
+  (->> canvas
+       (map clojure.string/join)
+       (clojure.string/join "\n")
+       println))
+
+
+(defn generate-filled-grid [serial size]
+  (let [new-canvas   (blank-canvas size)
+        power-levels (get-power-levels serial size)]
+    (reduce (fn [canvas x] (put-one-in-grid canvas x))
+            new-canvas
+            power-levels)))
+
+;; not sure if this works with 0 coords
+;; but we just leave an empty column/row at 0,0
+(defn calc-subgrid-coords [{:keys [x y] :as coord}]
+  "Returns the coords for a 3x3 grid (starting at the left-top.)"
+  (for [x (range x (+ x 3))
+        y (range y (+ y 3))]
+    {:x x :y y}))
+
+(defn- get-one-coord-val [grid {:keys [x y]}]
+  (nth (nth grid x) y))
+
+(defn- get-multi-coord-vals [grid coords]
+  (->> coords
+       (map #(get-one-coord-val grid %) ,,,)))
+
+
+(defn sum-all-vals [grid]
+  (reduce + (flatten grid)))
+
+
+(defn sum-a-sub-grid [grid {:keys [x y] :as coord}]
+  (->> (calc-subgrid-coords coord)
+       (get-multi-coord-vals grid ,,,)
+       (sum-all-vals)))
+
 
 (defn solve-part-1
-  [puz-in]
-  -1
+  [serial]
+  (let [fg (generate-filled-grid serial 300)]
+    (->> (for [x (range 100)                            ;; get all the subgrids (now just first 100)
+               y (range 100)]
+           {:coords {:x x :y y}
+            :sum    (sum-a-sub-grid fg { :x x :y y})})  ;; get the sums of all the subgrids
+         (sort-by :sum)                                 ;; sort
+         last                                           ;; biggest one, maybe use max-key here?
+         :sum)))
+
+(comment
+  ;;to get one subgrid from a filled-grid ww
+  (get-multi-coord-vals ww (calc-subgrid-coords {:x 33 :y 45}))
+
+  ;;assoc in grid (vec of vecs)
+  (def ex1 (assoc-in (blank-canvas 10) [2 2] "2"))
+  (print-grid ex1)
+  ;;__________
+  ;;__________
+  ;;__2_______
+  ;;__________
+  ;;__________
+  ;;__________
+  ;;__________
+  ;;__________
+  ;;__________
+  ;;__________
   )
