1809e99bc6b313145e5d9ea2b8cf2066c2aeb7c6
Niels Renard <code.i.pushed@gmail.com>
2018-12-16 02:04:54 +0100

    day 11 starting

--- test/adventofcode_2018/day_11_test.clj
+++ test/adventofcode_2018/day_11_test.clj
@@ -1,3 +1,22 @@
 (ns adventofcode-2018.day-11-test
   (:require [adventofcode-2018.day-11 :as sut]
-            [clojure.test :as t]))
+            [clojure.test :refer [is are deftest]]))
+
+
+
+(deftest find-power-level-test
+  (are [input expected]
+      (= (sut/find-power-level input) expected)
+    [8 3 5]      4
+    [57 122 79]  -5
+    [39 217 196] 0
+    [71 101 153] 4
+    ))
+
+
+(deftest solve-part-1
+  ;;What is the X,Y coordinate of the top-left fuel cell
+  ;;of the 3x3 square with the largest total power?
+  (are [input expected]
+      (= (sut/solve-part-1 input) expected)
+    1 -1))
