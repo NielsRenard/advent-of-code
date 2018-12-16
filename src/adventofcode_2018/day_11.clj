820e009ff58257581b076fdf1c6c073d2467ea16
Niels Renard <place@holder.com>
2018-12-16 02:04:54 +0100

    day 11 starting

--- src/adventofcode_2018/day_11.clj
+++ src/adventofcode_2018/day_11.clj
@@ -1,7 +1,23 @@
 (ns adventofcode-2018.day-11
-  (:require [util :refer :all]))
+  (:require [util :refer :all]
+            [clojure.edn :as edn]))
 
-(def input (util/get-input))
+(def input 3999)
+
+(defn find-power-level
+  [[serial x y]]
+  (let [rack-id      (+ x 10)
+        power-level (->> rack-id
+                         (* y)
+                         (+ serial)
+                         (* rack-id)
+                         str
+                         (take-last 3)
+                         first
+                         str
+                         edn/read-string
+                         )]
+    (- power-level 5)))
 
 (defn solve-part-1
   [puz-in]
