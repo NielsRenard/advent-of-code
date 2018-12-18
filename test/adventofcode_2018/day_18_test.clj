85ea77fc3c716b5ec96cc26be8dd9562fd5628f7
Niels Renard <place@holder.com>
2018-12-18 18:53:31 +0100

    d18 starting

--- test/adventofcode_2018/day_18_test.clj
+++ test/adventofcode_2018/day_18_test.clj
@@ -1,3 +1,20 @@
 (ns adventofcode-2018.day-18-test
   (:require [adventofcode-2018.day-18 :as sut]
-            [clojure.test :as t]))
+            [clojure.test :refer [is are deftest]]))
+
+(def example-input
+  ".#.#...|#.
+  .....#|##|
+  .|..|...#.
+  ..|#.....#
+  #.#|||#|#|
+  ...#.||...
+  .|....|...
+  ||...#|.#|
+  |.||||..|.
+  ...#.|..|."
+  )
+
+(deftest
+  (are [input expected] (= expected (sut/run-one-step input))
+    1 -1))
