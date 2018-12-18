c663db0205573ae38fc753026326db4bfa592100
Niels Renard <code.i.pushed@gmail.com>
2018-12-18 18:53:31 +0100

    d18 starting

--- src/adventofcode_2018/day_18.clj
+++ src/adventofcode_2018/day_18.clj
@@ -1,4 +1,15 @@
 (ns adventofcode-2018.day-18
-  (:require [util :refer :all]))
+  (:require [util :refer :all]
+            [clojure.string :as string]))
 
 (def input (util/get-input))
+
+(def example-input
+  ".#.#...|#.\n.....#|##|\n.|..|...#.\n..|#.....#\n#.#|||#|#|\n...#.||...\n.|....|...\n||...#|.#|\n|.||||..|.\n...#.|..|.\n")
+
+(defn parse-area [input]
+  (->> input
+       (map seq)))
+
+(defn run-one-step [area]
+  -1)
