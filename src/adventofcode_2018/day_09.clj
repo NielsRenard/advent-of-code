61bfaf6c24c7036933ceb026c9fb2aad40c87ed5
Niels Renard <place@holder.com>
2018-12-19 20:22:02 +0100

    d9 d12 and d14 puzzle-input

--- src/adventofcode_2018/day_09.clj
+++ src/adventofcode_2018/day_09.clj
@@ -1,7 +1,7 @@
 (ns adventofcode-2018.day-09
   (:require [util :refer :all]))
 
-(def input (util/get-input))
+(def input "435 players; last marble is worth 71184 points")
 
 (defn solve-part-1
   [puz-in]
