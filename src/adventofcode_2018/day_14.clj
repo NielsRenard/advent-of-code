09ee9429a9b8a09ed68e4bfc770b922f4de70a31
Niels Renard <code.i.pushed@gmail.com>
2018-12-19 20:22:02 +0100

    d9 d12 and d14 puzzle-input

--- src/adventofcode_2018/day_14.clj
+++ src/adventofcode_2018/day_14.clj
@@ -1,4 +1,10 @@
 (ns adventofcode-2018.day-14
   (:require [util :refer :all]))
 
-(def input (util/get-input))
+(def input 440231)
+
+(defn solve-part-1 [puz-in elf1-pos elf2-pos]
+  (let [row        (seq puz-in)
+        elf1-steps (inc (nth row elf1-pos))
+        elf2-steps (inc (nth row elf2-pos))]
+    (println (str "row" row "," "elf1-steps" elf1-steps ", elf2-steps" elf2-steps "."))))
