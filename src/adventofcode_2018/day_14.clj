f8f629952411c2f9743f4cc31d93b9d5f6a043ad
Niels Renard <code.i.pushed@gmail.com>
2018-12-24 02:55:14 +0100

    day 14.1 *

--- src/adventofcode_2018/day_14.clj
+++ src/adventofcode_2018/day_14.clj
@@ -3,8 +3,31 @@
 
 (def input 440231)
 
-(defn solve-part-1 [puz-in elf1-pos elf2-pos]
-  (let [row        (seq puz-in)
-        elf1-steps (inc (nth row elf1-pos))
-        elf2-steps (inc (nth row elf2-pos))]
-    (println (str "row" row "," "elf1-steps" elf1-steps ", elf2-steps" elf2-steps "."))))
+(defn generate-recipes [puz-in elf1-pos elf2-pos n]
+  (let [
+        recipe-1-score (get puz-in elf1-pos)
+        recipe-2-score (get puz-in elf2-pos)
+        new-recipes    (->> (+ recipe-1-score recipe-2-score)
+                            str
+                            (map #(Character/getNumericValue %))
+                            (into puz-in))
+        l              (count new-recipes)
+        e1-new-pos     (mod (+ elf1-pos (inc recipe-1-score)) l) ;; prevent idx OOB
+        e2-new-pos     (mod (+ elf2-pos (inc recipe-2-score)) l)
+        ]
+    (if (< n 400000) ;; how high should this be?
+      (recur new-recipes
+             e1-new-pos
+             e2-new-pos
+             (inc n))
+      new-recipes)))
+
+(defn solve-part-1 [input]
+  ;; time (solve-part-1 input))
+  ;; "Elapsed time: 1985.910501 msecs"
+  ;; "1052903161"
+  (->> (generate-recipes [3 7] 0 1 0)
+       (drop input)
+       (take 10)
+       (apply str)
+       ))
