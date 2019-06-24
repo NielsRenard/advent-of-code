edd459c8c24d18126fd2687501fadf24b4af58e6
Niels Renard <mail@nielsrenard.com>
2019-06-24 21:23:25 +0200

    let's try parser combinators

--- advent-of-haskell-code/src/Year2016/Day01.hs
+++ advent-of-haskell-code/src/Year2016/Day01.hs
@@ -1,15 +1,44 @@
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE OverloadedStrings #-}
 
 module Year2016.Day01 () where
 
-import RIO
-import Prelude (print)
-import qualified RIO.ByteString as B
 
-main :: IO ()
+import           Prelude                      (print)
+import           RIO
+import           RIO.List as L
+import           RIO.Text                     as T
+import           Text.Parsec ((<|>))
+import           Text.ParserCombinators.ReadP 
+import Text.Parsec.String (Parser)
+import Data.Char
+
+
+
+main :: IO Text
 main = do
-    contents <- B.readFile "data/2016/input/input_2016_01_a.txt"
-    let contents' = show $ B.length contents
-    print contents
-    print contents'
+    contents <- readFileUtf8 "data/2016/input/input_2016_01_a.txt"
+    let ws = T.split (== ',') contents
+        fst = case (headMaybe ws) of
+          Just x  -> x
+          Nothing -> error "no"
+    print fst
+    pure fst
+
+
+data Step = Step { direction :: Char
+                 , stepCount :: Int } deriving Show
+
+
+--stepParser :: readP Int
+--stepParser  = do
+--  direction <- char 'N'
+--  stepCount <- numbers 1
+--  return stepCount
+
+isVowel :: Char -> Bool
+isVowel char =
+    L.any (char ==) "aouei"
+
+vowel :: ReadP Char
+vowel =
+    satisfy isVowel
