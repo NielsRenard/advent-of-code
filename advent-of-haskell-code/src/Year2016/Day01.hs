5188618ffec5c6bb0b91a452165573203f68f175
Niels Renard <place@holder.com>
2019-06-28 15:02:26 +0200

    Parse compass direction

--- advent-of-haskell-code/src/Year2016/Day01.hs
+++ advent-of-haskell-code/src/Year2016/Day01.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
-module Year2016.Day01 () where
-
+module Year2016.Day01
+  (
+  ) where
 
 import           Data.Char
 import           Prelude                      (head, print, read)
@@ -12,28 +13,47 @@ import           Text.ParserCombinators.ReadP
 
 main :: IO Text
 main = do
-    contents <- readFileUtf8 "data/2016/input/input_2016_01_a.txt"
-    let ws = T.split (== ',') contents
-        fst = case (headMaybe ws) of
+  contents <- readFileUtf8 "data/2016/input/input_2016_01_a.txt"
+  let ws = T.split (== ',') contents
+      fst =
+        case (headMaybe ws) of
           Just x  -> x
           Nothing -> error "no"
-    print fst
-    pure fst
+  print fst
+  pure fst
 
-data Step = Step { direction :: Char
-                 , amount    :: Int } deriving Show
+data Step = Step
+  { direction :: Compass
+  , amount    :: Int
+  } deriving (Show)
+
+data Compass
+  = North
+  | East
+  | South
+  | West
+  deriving (Show)
 
 step :: ReadP Step
 step = do
-  direction <- char 'N' <|> char 'W' <|> char 'S' <|> char 'E'
-  steps <- count 1 digit <|> count 2 digit <|> count 3 digit
-  return Step {direction = direction, amount = read steps}
+  direction <- compass
+  steps <- numbers
+  return Step {direction = direction, amount = steps}
+
+compass :: ReadP Compass
+compass = do
+  c' <- char 'N' <|> char 'W' <|> char 'S' <|> char 'E'
+  return
+    (case c' of
+       'N' -> North
+       'E' -> East
+       'S' -> South
+       'W' -> West)
 
 digit :: ReadP Char
 digit = satisfy isDigit
 
-step :: ReadP (Char, Int)
-step = do
-  direction <- char 'N' <|> char 'W' <|> char 'S' <|> char 'E'
-  steps <- count 2 digit
-  return (direction, read steps)
+numbers :: ReadP Int
+numbers = do
+  n <- count 1 digit <|> count 2 digit <|> count 3 digit
+  return (read n)
