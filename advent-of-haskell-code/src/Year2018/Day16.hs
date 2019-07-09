1526c885708b74d3e6d5e8b54dae74f13cd700ec
Niels Renard <mail@nielsrenard.com>
2019-07-09 19:31:06 +0200

    Brittany for haskell formatting. 2016 day 01

--- advent-of-haskell-code/src/Year2018/Day16.hs
+++ advent-of-haskell-code/src/Year2018/Day16.hs
@@ -2,10 +2,14 @@ module Year2018.Day16 where
 
 main = print "hey"
 
+data Instruction = Instruction
+  { opcode :: Opcode
+  , inputs :: (Int, Int)
+  , output :: Int
+  }
 
-data Instruction = Instruction{ opcode :: Opcode, inputs :: (Int, Int) , output :: Int}
-
-data Opcode = Int
+data Opcode =
+  Int
 
 readInts :: String -> [Int]
 readInts = map (read . filter (/= '+')) . lines
