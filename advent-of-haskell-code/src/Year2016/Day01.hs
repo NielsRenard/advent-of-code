498feaf3720a389cc1ea1ae54297d853880f7a59
Niels Renard <mail@nielsrenard.com>
2019-07-09 19:31:06 +0200

    Brittany for haskell formatting. 2016 day 01

--- advent-of-haskell-code/src/Year2016/Day01.hs
+++ advent-of-haskell-code/src/Year2016/Day01.hs
@@ -1,33 +1,32 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Year2016.Day01
-  (
-  ) where
+  ()
+where
 
 import           Data.Char
 import           Data.Maybe
-import           Prelude                      (head, print, read)
-import           RIO hiding (many)
-import           RIO.List                     as L
-import           RIO.Text                     as T
+import           Prelude                        ( head
+                                                , print
+                                                , read
+                                                )
+import           RIO                     hiding ( many )
+import           RIO.List                      as L
+import           RIO.Text                      as T
 import           Text.ParserCombinators.ReadP
 
 main :: IO [(Move, String)]
 main = do
   contents <- readFileUtf8 "data/2016/input/input_2016_01_a.txt"
-  let ws = splitCommaAndStrip contents
-      all = L.map (head . tryToParse move . T.unpack)  ws
-  print ws
-  pure all
+  let ws    = splitCommaAndStrip contents
+      all   = L.map (head . readP_to_S move . T.unpack) ws
+      three = L.take 5 all
+  pure three
 
-splitCommaAndStrip :: Text -> [Text]
-splitCommaAndStrip = L.map T.strip . T.split (== ',')
-
-tryToParse parser = readP_to_S parser
-
+--  print ws
 data Move = Move
   { direction :: Turn
-  , steps     :: Int
+  , steps :: Int
   } deriving (Show)
 
 data Turn
@@ -35,24 +34,60 @@ data Turn
   | TurnRight
   deriving (Show)
 
+data Compass
+  = North
+  | East
+  | South
+  | West
+
+data Coordinate = Coordinate
+  { x :: Int
+  , y :: Int
+  } deriving (Show)
+
+translate :: Coordinate -> Compass -> Move -> Coordinate
+translate coord compass move =
+  let x' = x coord
+      y' = y coord
+      n  = steps move
+  in  case direction move of
+        TurnLeft -> case compass of
+          North -> Coordinate (x' - n) y'
+          East  -> Coordinate x' (y' + n)
+          South -> Coordinate (x' - n) y'
+          West  -> Coordinate x' (y' - n)
+        TurnRight -> case compass of
+          North -> Coordinate (x' + n) y'
+          East  -> Coordinate x' (y' + n)
+          South -> Coordinate (x' + n) y'
+          West  -> Coordinate x' (y' + n)
+
+-- parser of Moves
 move :: ReadP Move
 move = do
   direction <- turn
-  steps <- numbers
-  return (Move direction steps)
+  steps     <- numbers
+  return $ Move direction steps
 
+-- parser of Turns
 turn :: ReadP Turn
 turn = do
   t' <- char 'L' <|> char 'R'
   return
     (case t' of
-       'L' -> TurnLeft
-       'R' -> TurnRight)
+      'L' -> TurnLeft
+      'R' -> TurnRight
+    )
 
+-- parser of digits
 digit :: ReadP Char
 digit = satisfy isDigit
 
+-- parser of numbers max 3 digits long
 numbers :: ReadP Int
 numbers = do
   n <- count 1 digit <|> count 2 digit <|> count 3 digit
   return (read n)
+
+splitCommaAndStrip :: Text -> [Text]
+splitCommaAndStrip = L.map T.strip . T.split (== ',')
