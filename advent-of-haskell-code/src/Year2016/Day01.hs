96f656504e6a01baf99e686e57876aea869a4794
Niels Renard <place@holder.com>
2019-07-09 21:55:31 +0200

    2016 d1 a *

--- advent-of-haskell-code/src/Year2016/Day01.hs
+++ advent-of-haskell-code/src/Year2016/Day01.hs
@@ -6,7 +6,7 @@ where
 
 import           Data.Char
 import           Data.Maybe
-import           Prelude                        ( head
+import           Prelude                        ( last
                                                 , print
                                                 , read
                                                 )
@@ -19,25 +19,24 @@ main :: IO Int
 main = do
   contents <- readFileUtf8 "data/2016/input/input_2016_01_a.txt"
   let
-    exampleString = T.pack "R2, R2, R2"
---    ws    = splitCommaAndStrip exampleString   -- flip next to line comments
-    ws    = splitCommaAndStrip contents      -- to test with hardcoded string
-    all   = L.map (fst . head . readP_to_S move . T.unpack) ws
-    first = head all
+--    exampleString = T.pack "R2, L10"               -- should give 12
+--    ws    = splitCommaAndStrip exampleString       -- flip next two line comments
+    ws  = splitCommaAndStrip contents      -- to test with hardcoded string
+    all = L.map (fst . last . readP_to_S move . T.unpack) ws
     initPos =
       Position { coordinate = Coordinate { x = 0, y = 0 }, facing = North }
-    endPos = L.foldl walk initPos all
-    distance = taxicabDiff initPos endPos  -- {x = 18, y = -17} facing South
+    endPos   = L.foldl walk initPos all
+    distance = taxicabDiff initPos endPos  -- {x = -173, y = -159} facing South
   pure distance
 
 
 taxicabDiff :: Position -> Position -> Int
-taxicabDiff p1 p2  = let x1 = x $ coordinate p1
-                         x2 = x $ coordinate p2
-                         y1 = y $ coordinate p1
-                         y2 = y $ coordinate p2
-                     in
-                       abs (x1 - x2) + abs (y1 - y2)
+taxicabDiff p1 p2 =
+  let x1 = x $ coordinate p1
+      x2 = x $ coordinate p2
+      y1 = y $ coordinate p1
+      y2 = y $ coordinate p2
+  in  (abs (x1 - x2)) + (abs (y1 - y2))
 
 walk :: Position -> Move -> Position
 walk position m = translate position m
@@ -110,7 +109,7 @@ digit = satisfy isDigit
 -- parser of numbers max 3 digits long
 numbers :: ReadP Int
 numbers = do
-  n <- count 1 digit <|> count 2 digit <|> count 3 digit
+  n <- count 3 digit <|> count 2 digit <|> count 1 digit
   return (read n)
 
 splitCommaAndStrip :: Text -> [Text]
