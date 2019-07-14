4636afccd0a14adc64c18039c73d3cb2bc6bafb3
Niels Renard <place@holder.com>
2019-07-14 14:20:05 +0200

    2016 d1 **

--- advent-of-haskell-code/src/Year2016/Day01.hs
+++ advent-of-haskell-code/src/Year2016/Day01.hs
@@ -12,23 +12,41 @@ import           Prelude                        ( last
                                                 )
 import           RIO                     hiding ( many )
 import           RIO.List                      as L
+import           RIO.List.Partial              as L'
 import           RIO.Text                      as T
 import           Text.ParserCombinators.ReadP
+
+
+{--
+part 1
+Wanted to learn how to use Parser Combinators so did that. Still hardcoding the max three digits for the numbers parser. Think I can change that by using many1.
+
+part 2
+Read the assignment wrong initially, thinking it was looking for the first coordinate that it *lands* (and turns) on twice.
+--}
 
 main :: IO Int
 main = do
   contents <- readFileUtf8 "data/2016/input/input_2016_01_a.txt"
   let
---    example = T.pack "R2, L10"                       -- should give 12
---    ws            = splitCommaAndStrip example       -- uncomment to test with hardcoded input
-    ws  = splitCommaAndStrip contents
-    all = L.map (fst . last . readP_to_S move . T.unpack) ws
-    initPos =
-      Position { coordinate = Coordinate { x = 0, y = 0 }, facing = North }
-    endPos   = L.foldl translate initPos all
-    distance = taxicabDiff initPos endPos  -- {x = -173, y = -159} facing South
-  pure distance
+--      example = T.pack "R1, R1, R1, R1, R1, R1, R1"
+--      ws            = splitCommaAndStrip example       -- uncomment to test with hardcoded input
+      ws      = splitCommaAndStrip contents
+      all     = L.map (fst . last . readP_to_S move . T.unpack) ws
+      initPos = Position { coordinate = Coordinate { x = 0, y = 0 }
+                         , facing     = North
+                         , visited    = []
+                         }
+      endPos   = L.foldl translate initPos all
+      distance = taxicabDiff initPos endPos
+      cs       = visitedTwice $ visited endPos
+  pure $ taxicabDiff initPos $ Position (last cs) North []
 
+visitedTwice :: [Coordinate] -> [Coordinate]
+visitedTwice cols =
+  let uniqueCols = nub $ L.reverse cols
+      diff       = cols \\ uniqueCols
+  in  diff
 
 taxicabDiff :: Position -> Position -> Int
 taxicabDiff p1 p2 =
@@ -36,11 +54,12 @@ taxicabDiff p1 p2 =
       x2 = xco p2
       y1 = yco p1
       y2 = yco p2
-  in  abs (x1 - x2) + abs (y1 - y2)
+  in  cartDiff x1 x2 y1 y2
  where
   xco p = x $ coordinate p
   yco p = y $ coordinate p
 
+cartDiff x1 x2 y1 y2 = abs (x1 - x2) + abs (y1 - y2)
 
 data Move = Move
   { direction :: Turn
@@ -61,30 +80,45 @@ data Compass
 
 data Position = Position { coordinate :: Coordinate
                          , facing :: Compass
+                         , visited :: [Coordinate]
                          } deriving (Show)
 
 data Coordinate = Coordinate
   { x :: Int
   , y :: Int
-  } deriving (Show)
+  } deriving (Show, Eq)
 
 translate :: Position -> Move -> Position
 translate position move =
   let x'      = x $ coordinate position
       y'      = y $ coordinate position
+      v'      = passing position move ++ visited position
       compass = facing position
       n       = steps move
   in  case direction move of
         TurnLeft -> case compass of
-          North -> Position (Coordinate (x' - n) y') West
-          East  -> Position (Coordinate x' (y' + n)) North
-          South -> Position (Coordinate (x' + n) y') East
-          West  -> Position (Coordinate x' (y' - n)) South
+          North -> Position (Coordinate (x' - n) y') West v'
+          East  -> Position (Coordinate x' (y' + n)) North v'
+          South -> Position (Coordinate (x' + n) y') East v'
+          West  -> Position (Coordinate x' (y' - n)) South v'
         TurnRight -> case compass of
-          North -> Position (Coordinate (x' + n) y') East
-          East  -> Position (Coordinate x' (y' - n)) South
-          South -> Position (Coordinate (x' - n) y') West
-          West  -> Position (Coordinate x' (y' + n)) North
+          North -> Position (Coordinate (x' + n) y') East v'
+          East  -> Position (Coordinate x' (y' - n)) South v'
+          South -> Position (Coordinate (x' - n) y') West v'
+          West  -> Position (Coordinate x' (y' + n)) North v'
+
+passing :: Position -> Move -> [Coordinate]
+passing p m =
+  let
+    newPos = translate p m
+    oldX   = x $ coordinate p
+    oldY   = y $ coordinate p
+    newX   = x $ coordinate newPos
+    newY   = y $ coordinate newPos
+    passX  = if oldX < newX then [oldX .. newX] else L.reverse [newX .. oldX]
+    passY  = if oldY < newY then [oldY .. newY] else L.reverse [newY .. oldY]
+  in
+    L'.tail [ Coordinate { x = x', y = y' } | x' <- passX, y' <- passY ]
 
 -- parser of Moves
 move :: ReadP Move
