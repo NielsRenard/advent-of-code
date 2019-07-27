dffd0ddd8d95957ff16c662a838311f33ce99fba
Niels Renard <place@holder.com>
2019-07-27 16:21:04 +0200

    2016 d5 **

--- advent-of-haskell-code/src/Year2016/Day05.hs
+++ advent-of-haskell-code/src/Year2016/Day05.hs
@@ -21,21 +21,26 @@ import Data.Digest.Pure.MD5
 Felt like playing with RIO Text so a lot of clunky string conversion going on.
 --}
 
-{--
-answerOne input [1..10000000]
-[(True,702868),(True,1776010),(True,8421983),(True,8744114),(True,8845282),(True,9268910),(True,9973527)]
---}
-
--- answer takes a good twenty seconds now
--- something something with isFirstFiveZero, immediately False out if a char isn't 0
+-- get rid of hardcoded ranges by making it look UNTIL it finds 8 valid characters
 answerOne = getAnswer input [702868..12000000]
+-- takes long to run but eventually completes
+answerTwo = getAnswer input [702868..30000000]
 
+-- TODO: Only use the first hit for any position
+-- "f12f9cc73009de6f75" -> "f2c730e5"
 getAnswer :: TL.Text -> [Int] -> String
 getAnswer inp nums =
   L.map (\n -> let hash' = concatHash inp n
-               in (isFirstFiveZero hash', TL.unpack hash' !! 5)) nums
-  & L.filter ((== True) . fst)
-  & map snd
+               in (isFirstFiveZero hash', TL.unpack hash' !! 5, TL.unpack hash' !! 6)) nums
+  & L.filter ((== True) . (\(a,b,c) -> a))
+  & L.filter (\(a,b,c) -> isValidPosition b)
+  & L.sortBy (compare `on` (\(a,b,c)->b))
+  & L.map (\(a,b,c) -> c)
+
+
+isValidPosition :: Char -> Bool
+isValidPosition c = isDigit c && (read [c] < 8)
+
 
 isFirstFiveZero :: TL.Text -> Bool
 isFirstFiveZero t = TL.take 5 t == TL.pack ([1..5] *> "0")
