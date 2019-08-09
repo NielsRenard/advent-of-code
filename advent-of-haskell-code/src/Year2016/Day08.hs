a99666dccd9de22050a9820eb32dc5deccb643b0
Niels Renard <code.i.pushed@gmail.com>
2019-08-09 15:11:42 +0200

    make format

--- advent-of-haskell-code/src/Year2016/Day08.hs
+++ advent-of-haskell-code/src/Year2016/Day08.hs
@@ -17,7 +17,7 @@ import qualified RIO.List.Partial              as L'
 import qualified RIO.Map                       as M
 import qualified RIO.Set                       as S
 import qualified RIO.Text.Lazy                 as TL
-import           Text.Pretty.Simple (pPrint)
+import           Text.Pretty.Simple             ( pPrint )
 
 data Pixel = Pixel { lit :: Bool, x :: Int, y :: Int } deriving (Show)
 type Screen = [Pixel]
@@ -25,15 +25,16 @@ type Width = Int
 type Height = Int
 
 initScreen :: Width -> Height -> Screen
-initScreen w h = [Pixel False x' y' | x' <- [0..w] , y' <- [0..h]]
+initScreen w h = [ Pixel False x' y' | x' <- [0 .. w], y' <- [0 .. h] ]
 
 renderScreen :: Screen -> String
-renderScreen s = let maxHeight = y $ L'.maximumBy (comparing y) s
-                     allRows = [getRow s y' | y' <- [0..maxHeight]]
-                 in unlines $ L.map (L.map renderPixel) allRows
+renderScreen s =
+  let maxHeight = y $ L'.maximumBy (comparing y) s
+      allRows   = [ getRow s y' | y' <- [0 .. maxHeight] ]
+  in  unlines $ L.map (L.map renderPixel) allRows
 
 
-getRow pxs rowNum = L.filter (\p -> y p == rowNum ) pxs
+getRow pxs rowNum = L.filter (\p -> y p == rowNum) pxs
 
 renderPixel :: Pixel -> Char
 renderPixel p = if lit p then '#' else '.'
@@ -44,8 +45,14 @@ renderPixel p = if lit p then '#' else '.'
       .##
 -}
 scr0 :: Screen
-scr0 = [Pixel False 0 0, Pixel True 1 0, Pixel True 2 0,
-        Pixel False 0 1, Pixel True 1 1, Pixel True 2 1]
+scr0 =
+  [ Pixel False 0 0
+  , Pixel True  1 0
+  , Pixel True  2 0
+  , Pixel False 0 1
+  , Pixel True  1 1
+  , Pixel True  2 1
+  ]
 
-p0 = Pixel {lit = False, x = 0, y = 0}
-p1 = Pixel {lit = True, x = 0, y = 0}
+p0 = Pixel { lit = False, x = 0, y = 0 }
+p1 = Pixel { lit = True, x = 0, y = 0 }
