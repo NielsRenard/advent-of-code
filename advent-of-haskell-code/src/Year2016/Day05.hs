94c53d18f0329236355832df880326318dbc31cb
Niels Renard <niels@nielsrenard.com>
2019-12-01 13:21:07 +0100

    Reformat everything with ormolu

--- advent-of-haskell-code/src/Year2016/Day05.hs
+++ advent-of-haskell-code/src/Year2016/Day05.hs
@@ -1,22 +1,24 @@
 {-# LANGUAGE NoImplicitPrelude #-}
 
 module Year2016.Day05
-  ()
+  (
+  )
 where
 
-import           Data.Digest.Pure.MD5
-import qualified Data.List.Split               as Split
-import           Data.Text.Lazy.Encoding       as Enc
-import           Prelude                        ( head
-                                                , read
-                                                , (!!)
-                                                )
-import           RIO
-import           RIO.Char                       ( isDigit )
-import qualified RIO.List                      as L
-import qualified RIO.Map                       as M
-import qualified RIO.Set                       as S
-import qualified RIO.Text.Lazy                 as TL
+import Data.Digest.Pure.MD5
+import qualified Data.List.Split as Split
+import Data.Text.Lazy.Encoding as Enc
+import RIO
+import RIO.Char (isDigit)
+import qualified RIO.List as L
+import qualified RIO.Map as M
+import qualified RIO.Set as S
+import qualified RIO.Text.Lazy as TL
+import Prelude
+  ( (!!),
+    head,
+    read,
+  )
 
 {--
 Felt like playing with RIO Text so a lot of clunky string conversion going on.
@@ -24,17 +26,18 @@ Felt like playing with RIO Text so a lot of clunky string conversion going on.
 
 -- get rid of hardcoded ranges by making it look UNTIL it finds 8 valid characters
 answerOne = getAnswer input [702868 .. 12000000]
+
 -- takes long to run but eventually completes
 answerTwo = getAnswer input [702868 .. 30000000]
 
 getAnswer :: TL.Text -> [Int] -> String
 getAnswer inp nums =
   L.map
-      (\n ->
+    ( \n ->
         let hash' = concatHash inp n
-        in  (isFirstFiveZero hash', TL.unpack hash' !! 5, TL.unpack hash' !! 6)
-      )
-      nums
+         in (isFirstFiveZero hash', TL.unpack hash' !! 5, TL.unpack hash' !! 6)
+    )
+    nums
     & L.filter ((== True) . (\(a, b, c) -> a))
     & L.filter (\(a, b, c) -> isValidPosition b)
     & L.map (\(a, b, c) -> (b, c))
@@ -42,10 +45,8 @@ getAnswer inp nums =
     & L.sortBy (compare `on` fst)
     & L.map snd
 
-
 isValidPosition :: Char -> Bool
 isValidPosition c = isDigit c && (read [c] < 8)
-
 
 isFirstFiveZero :: TL.Text -> Bool
 isFirstFiveZero t = TL.all (== '0') (TL.take 5 t)
@@ -55,5 +56,7 @@ concatHash :: TL.Text -> Int -> TL.Text
 concatHash t n = TL.pack . show . md5 . Enc.encodeUtf8 $ t <> TL.pack (show n)
 
 myHash = md5 $ Enc.encodeUtf8 input
+
 input = TL.pack "ugkcyxxp"
+
 exampleInput = TL.pack "abc"
--- advent-of-haskell-code/src/Year2018/Day16.hs
+++ advent-of-haskell-code/src/Year2018/Day16.hs
@@ -2,14 +2,15 @@ module Year2018.Day16 where
 
 main = print "hey"
 
-data Instruction = Instruction
-  { opcode :: Opcode
-  , inputs :: (Int, Int)
-  , output :: Int
-  }
+data Instruction
+  = Instruction
+      { opcode :: Opcode,
+        inputs :: (Int, Int),
+        output :: Int
+      }
 
-data Opcode =
-  Int
+data Opcode
+  = Int
 
 readInts :: String -> [Int]
 readInts = map (read . filter (/= '+')) . lines
