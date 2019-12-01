c687a1a386f244fdfd580ab7d83c490e93ad3566
Niels Renard <code.i.pushed@gmail.com>
2019-12-01 21:39:32 +0100

    ㏠ ☆☆ clojure solution

---
+++ advent-of-clojure-code/src/adventofcode_2019/day_01.clj
@@ -0,0 +1,20 @@
+(ns adventofcode-2019.day-01
+  (:require [util :refer :all]
+            [clojure.edn :as edn]))
+
+(def input (->> (util/get-input)
+                (map edn/read-string)))
+
+(defn calcFuel [mass]
+  (- (Math/floor (int (/ mass 3))) 2))
+
+(defn recursiveFuel [mass]
+  (if (> (calcFuel mass) 0)
+    (+ (calcFuel mass) (recursiveFuel (calcFuel mass)))
+    0))
+
+(defn solve-part-1 [puz-in]
+  (reduce + (map calcFuel puz-in)))
+
+(defn solve-part-2 [puz-in]
+  (reduce + (map recursiveFuel puz-in)))
