ac3bd17ea6ac258a2e401875fc4577c0906f4f23
Niels Renard <code.i.pushed@gmail.com>
2019-12-06 00:51:49 +0100

    I think this is the right direction

--- advent-of-haskell-code/src/Utils.hs
+++ advent-of-haskell-code/src/Utils.hs
@@ -2,12 +2,18 @@
 
 module Utils
   ( frequencies,
+    digits
   )
 where
 
 import RIO
 import qualified RIO.List as L
 import qualified RIO.Map as M
+import Data.Char (digitToInt)
+import           Prelude                        ( read )
 
 frequencies :: (Ord k, Num a) => [k] -> [(k, a)]
 frequencies xs = M.toList $ M.fromListWith (+) [(c, 1) | c <- xs]
+
+digits :: Int -> [Int]
+digits = map (read . return) . show
--- advent-of-haskell-code/src/Year2019/Day05.hs
+++ advent-of-haskell-code/src/Year2019/Day05.hs
@@ -4,6 +4,7 @@ module Year2019.Day05 where
 -- this puzzle continues from Day02
 import Data.List
 import Data.List.Index
+import Utils
 import Data.Function ((&))
 
 -- (...) it will run an output instruction indicating how far the result  was from the expected value
@@ -32,15 +33,25 @@ slurp xs index acc =
     else slurp xs' index' acc'
   where
     opCode = xs !! index
-    firstArg = xs !! (index + 1)
-    secondArg = xs !! (index + 2)
+    firstArgPos = xs !! (xs !! (index + 1))
+    secondArgPos = xs !! (xs !! (index + 2))
+    thirdArg = xs !! (index + 3)
+    firstArgImmediate = xs !! (index + 1)
+    secondArgImmediate = xs !! (index + 2)
+    args = case digits opCode of
+      [1] -> firstArgPos + secondArgPos
+      [2] -> firstArgPos * secondArgPos
+      [0,0,0,1] -> firstArgPos + secondArgPos
+      [0,1,0,1] -> firstArgImmediate + secondArgPos
+      [1,1,0,1] -> firstArgImmediate + secondArgImmediate
+      [0,0,0,2] -> firstArgPos * secondArgPos
+      [0,1,0,2] -> firstArgImmediate * secondArgPos
+      [1,1,0,2] -> firstArgImmediate * secondArgImmediate
     index' | opCode `elem` [1,2] = index + 4
            | opCode `elem` [3,4] = index + 2
            | otherwise = length xs
-    xs'  | opCode == 1 = setAt index
-           (firstArg + secondArg) xs
-         | opCode == 2 = setAt index
-           (firstArg * secondArg) xs
+    xs'  | opCode == 1 = setAt thirdArg args xs
+         | opCode == 2 = setAt thirdArg args xs
          | opCode == 99 = xs
          | opCode > 999 && opCode < 10000  = xs -- catches everything, now rebuild the whole thing to support "mode parameters"
     acc' | opCode == 4 =
@@ -72,4 +83,3 @@ input=[3,225,1,225,6,6,1100,1,238,225,104,0,1102,17,65,225,102,21,95,224,1001,22
 
 inputDayTwo :: [Int]
 inputDayTwo = [1, 0, 0, 3, 1, 1, 2, 3, 1, 3, 4, 3, 1, 5, 0, 3, 2, 1, 10, 19, 1, 6, 19, 23, 1, 10, 23, 27, 2, 27, 13, 31, 1, 31, 6, 35, 2, 6, 35, 39, 1, 39, 5, 43, 1, 6, 43, 47, 2, 6, 47, 51, 1, 51, 5, 55, 2, 55, 9, 59, 1, 6, 59, 63, 1, 9, 63, 67, 1, 67, 10, 71, 2, 9, 71, 75, 1, 6, 75, 79, 1, 5, 79, 83, 2, 83, 10, 87, 1, 87, 5, 91, 1, 91, 9, 95, 1, 6, 95, 99, 2, 99, 10, 103, 1, 103, 5, 107, 2, 107, 6, 111, 1, 111, 5, 115, 1, 9, 115, 119, 2, 119, 10, 123, 1, 6, 123, 127, 2, 13, 127, 131, 1, 131, 6, 135, 1, 135, 10, 139, 1, 13, 139, 143, 1, 143, 13, 147, 1, 5, 147, 151, 1, 151, 2, 155, 1, 155, 5, 0, 99, 2, 0, 14, 0]
-
