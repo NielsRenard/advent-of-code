25387aa071322fbd8808f7529d881759b1a0e3b9
Niels Renard <code.i.pushed@gmail.com>
2019-12-08 21:44:33 +0100

    ㏧ ⋆⋆

--- advent-of-haskell-code/src/Year2019/Day08.hs
+++ advent-of-haskell-code/src/Year2019/Day08.hs
@@ -8,47 +8,29 @@ import Data.List.Split (chunksOf)
 import Utils
 import Year2019.Input.Day08 (input)
 
-exampleInput :: [Integer]
-exampleInput = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2]
-
-myExampleInput :: [Integer]
-myExampleInput = [1, 2, 3, 0, 0, 0, 7, 8, 9, 0, 1, 2]
-
-partTwoExInput :: [Integer]
-partTwoExInput = [0, 2, 2, 2, 1, 1, 2, 2, 2, 2, 1, 2, 0, 0, 0, 0]
-
 solvePartOne =
   let layers = layers25x6 input
       fewestZerosLayer = layers !! fst (head $ sortOn snd $ indexed $ map countZerosInLayer layers)
-      ones = countOnesInLayer fewestZerosLayer
-      twos = countTwosInLayer fewestZerosLayer
+      ones = length . concatMap (filter (== 1)) $ fewestZerosLayer
+      twos = length . concatMap (filter (== 2)) $ fewestZerosLayer
    in ones * twos
+
+solvePartTwo =
+  renderLayer $ chunksOf 25 $ allLayersPixels (layers25x6 input) 25 6
+
+allLayersPixels layers width height =  [visiblePixel $ map ((!! that) . map (!! it)) layers |
+                                         that <- [0..pred height],
+                                         it <- [0..pred width]]
 
 visiblePixel :: [Integer] -> Integer
 visiblePixel stackedPixel = head $ dropWhile (== 2) stackedPixel
 
 layers25x6 = chunksOf 6 . chunksOf 25
 
-layers3x2 = chunksOf 2 . chunksOf 3
-
-layers2x2 = chunksOf 2 . chunksOf 2
-
 -- silly that these aren't just one function ---------------------
 countZerosInLayer :: (Eq a, Num a) => [[a]] -> Int
 countZerosInLayer = length . concatMap (filter (== 0))
 
-
-countOnesInLayer :: (Eq a, Num a) => [[a]] -> Int
-countOnesInLayer = length . concatMap (filter (== 1))
-
-
-countTwosInLayer :: (Eq a, Num a) => [[a]] -> Int 
-countTwosInLayer = length . concatMap (filter (== 2))
-------------------------------------------------------------------
-
--- don't need this
-allZeros :: (Eq a, Num a) => Int -> [[a]] -> Int
-allZeros width = length . filter (== replicate width 0)
 
 -- render functions ----------------------------------------------------
 renderLayer :: [[Integer]] -> IO ()
@@ -56,7 +38,7 @@ renderLayer layer = mapM_ putStrLn $ (map $ map renderPixel) layer
 
 renderPixel :: Integer -> Char
 renderPixel x = case x of
-  0 -> '*'
-  1 -> '_'
+  0 -> ' '
+  1 -> '#'
   2 -> '?'
 ------------------------------------------------------------------------
