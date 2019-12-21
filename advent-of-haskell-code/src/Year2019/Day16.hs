bf2c27a91a53519f4a4cd839c3b94ea97db938ff
Niels Renard <code.i.pushed@gmail.com>
2019-12-21 13:46:59 +0100

    input day16

---
+++ advent-of-haskell-code/src/Year2019/Day16.hs
@@ -0,0 +1,11 @@
+{-# LANGUAGE OverloadedStrings #-}
+module Year2019.Day16 where
+
+
+import Year2019.Input.Day16 (input)
+import Data.Function ((&))
+import Data.Text as T (Text, unpack, pack)
+import Data.List as L
+import Data.List.Index
+import Data.List.Split (chunksOf)
+import Utils
