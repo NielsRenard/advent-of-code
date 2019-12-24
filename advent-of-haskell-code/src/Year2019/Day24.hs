17ebb94e9da4ece469247e5a1e44fb02c1a2e980
Niels Renard <code.i.pushed@gmail.com>
2019-12-24 13:19:16 +0100

    wip day 24

---
+++ advent-of-haskell-code/src/Year2019/Day24.hs
@@ -0,0 +1,70 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Year2019.Day24 where
+
+import Data.Function ((&))
+import Data.List.Split
+import Data.List as L
+import Data.Text as T
+import Utils
+
+
+type Grid = [Text]
+type Tile = Char
+type X = Int
+type Y = Int
+
+tick :: Grid -> Grid
+tick g =
+  T.chunksOf 5 $ T.pack [transformTile (x,y) g | y <- [0..4], x <- [0..4]]
+
+transformTile :: (X,Y) -> Grid -> Tile
+transformTile (x,y) g =
+  let adjacent = getAdjacent (x,y) g
+      adjacentBugCount =  L.length (L.filter (== '#') adjacent)
+  in
+  case getTile (x,y) g of
+    '#' -> 
+      if adjacentBugCount == 1 then '#' else '.'
+    '.' ->
+      if adjacentBugCount `elem` [1,2] then '#' else '.'
+
+getAdjacent :: (X,Y) -> Grid -> [Tile]
+getAdjacent (x,y) g =
+  let
+    down = getTile (x,y+1) g
+    left = getTile (x-1,y) g
+    right = getTile (x+1,y) g
+    up = getTile (x,y-1) g
+  in [up, left, right, down]
+  
+getTile :: (X,Y) -> Grid -> Tile
+getTile (x,y) g = if x `elem` [0..4] && y `elem` [0..4]
+                  then T.index (g !! y) x
+                  else '.'
+
+
+invertChar :: Char -> Char
+invertChar '#' = '.'
+invertChar '.' = '#'
+
+print' :: [Text] -> IO()
+print' = mapM_ print
+
+
+exIn1 :: [Text]
+exIn1 =
+  [ "....#",
+    "#..#.",
+    "#..##",
+    "..#..",
+    "#...."
+  ]
+
+input :: [Text]
+input =
+  ["####.",
+   ".###.",
+   ".#..#",
+   "##.##",
+   "###.."]
