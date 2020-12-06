1ea13e0194eb9ee4d5b081493617218dd3142ce9
Niels Renard <code.i.pushed@gmail.com>
2020-12-06 08:50:14 +0100

    2020 d6 **

--- advent-of-haskell-code/src/Year2020/Day06.hs
+++ advent-of-haskell-code/src/Year2020/Day06.hs
@@ -19,30 +19,53 @@ import System.Directory
 import Utils
 
 solvePart1 input =
-  let x = 1
-   in f x
-  where
-    f = id
+  let uniqueAnswersPerGroup =
+        L.map (S.fromList . T.unpack)
+        $ L.map (T.replace " " "" )
+        $ L.map T.unwords
+        $ splitWhen (== "") input
+  in
+    L.foldr ((+) . L.length) 0 uniqueAnswersPerGroup
 
 solvePart2 input =
-  let x = 1
-   in f x
-  where
-    f = id
+  let answersPerGroup :: [[Text]] = splitWhen (== "") input
+      listOfListsOfAnswers :: [[String]] = (L.map (L.map T.unpack)) answersPerGroup
+  in
+    foldr (+) 0
+    $ L.map L.length
+    $ L.map (L.foldl1 S.intersection)
+    $ L.map (L.map S.fromList) listOfListsOfAnswers
 
 main :: IO ()
 main = do
   input <- map T.pack <$> lines <$> readFile "data/2020/6.input"
   let ex1 = solvePart1 exinp
   let answer1 = solvePart1 input
+  let ex2 = solvePart2 exinp  
   let answer2 = solvePart2 input
   putStrLn $ "Example 1: " <> show ex1
   putStrLn $ "   Part 1: " <> show answer1
+  putStrLn $ "Example 2: " <> show ex2  
   putStrLn $ "   Part 2: " <> show answer2
 
 {-- Test and example input --}
 
 exinp :: [Text]
 exinp =
-  [ "STUB"
+  [
+    "abc",
+    "",
+    "a",
+    "b",
+    "c",
+    "",
+    "ab",
+    "ac",
+    "",
+    "a",
+    "a",
+    "a",
+    "a",
+    "",
+    "b"
   ]
