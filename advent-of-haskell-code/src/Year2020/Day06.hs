2e5cd065d44389f1d2c5b657d004601fada31bbf
Niels Renard <code.i.pushed@gmail.com>
2020-12-07 23:05:41 +0100

    2020 d7 *

--- advent-of-haskell-code/src/Year2020/Day06.hs
+++ advent-of-haskell-code/src/Year2020/Day06.hs
@@ -20,40 +20,38 @@ import Utils
 
 solvePart1 input =
   let uniqueAnswersPerGroup =
-        L.map (S.fromList . T.unpack)
-        $ L.map (T.replace " " "" )
-        $ L.map T.unwords
-        $ splitWhen (== "") input
-  in
-    L.foldr ((+) . L.length) 0 uniqueAnswersPerGroup
+        L.map (S.fromList . T.unpack) $
+          L.map (T.replace " " "") $
+            L.map T.unwords $
+              splitWhen (== "") input
+   in L.foldr ((+) . L.length) 0 uniqueAnswersPerGroup
 
 solvePart2 input =
   let answersPerGroup :: [[Text]] = splitWhen (== "") input
-      listOfListsOfAnswers :: [[String]] = (L.map (L.map T.unpack)) answersPerGroup
-  in
-    foldr (+) 0
-    $ L.map L.length
-    $ L.map (L.foldl1 S.intersection)
-    $ L.map (L.map S.fromList) listOfListsOfAnswers
+      listOfListsOfSetsOfAnswers :: [[S.Set Char]] = (L.map (L.map (S.fromList . T.unpack))) answersPerGroup
+   in foldr (+) 0 $
+        L.map L.length $
+          L.map
+            (L.foldl1 S.intersection)
+            listOfListsOfSetsOfAnswers
 
 main :: IO ()
 main = do
   input <- map T.pack <$> lines <$> readFile "data/2020/6.input"
   let ex1 = solvePart1 exinp
   let answer1 = solvePart1 input
-  let ex2 = solvePart2 exinp  
+  let ex2 = solvePart2 exinp
   let answer2 = solvePart2 input
   putStrLn $ "Example 1: " <> show ex1
   putStrLn $ "   Part 1: " <> show answer1
-  putStrLn $ "Example 2: " <> show ex2  
+  putStrLn $ "Example 2: " <> show ex2
   putStrLn $ "   Part 2: " <> show answer2
 
 {-- Test and example input --}
 
 exinp :: [Text]
 exinp =
-  [
-    "abc",
+  [ "abc",
     "",
     "a",
     "b",
