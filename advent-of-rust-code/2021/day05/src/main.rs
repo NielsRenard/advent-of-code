5d6b0930f804e1ab186718843518b95e7007052a
Niels Renard <code.i.pushed@gmail.com>
2021-12-05 21:11:07 +0100

    2021/05 ** + visualisation

--- advent-of-rust-code/2021/day05/src/main.rs
+++ advent-of-rust-code/2021/day05/src/main.rs
@@ -1,5 +1,5 @@
 #![allow(dead_code, unused_mut, unused_variables, unused_imports)]
-#![feature(iter_advance_by, iter_zip)]
+#![feature(iter_advance_by, iter_zip, destructuring_assignment)]
 
 use std::{
     collections::{hash_map, HashMap},
@@ -17,11 +17,9 @@ fn main() {
     print_time!("execution");
     let answer_1 = solve_part_one(&input_from_file());
     println!("part 1: {}", answer_1);
-    // execution took 9.7ms
-
-    // let answer_2 = solve_part_two(&input_from_file());
-    // println!("part 2: {}", answer_2);
-    // execution took ?
+    let answer_2 = solve_part_two(&input_from_file());
+    println!("part 2: {}", answer_2);
+    // execution took 31.8ms
 }
 
 type Line = (Point, Point);
@@ -34,17 +32,33 @@ struct Point {
 
 fn solve_part_one(input: &str) -> i32 {
     let lines = input.lines().into_iter().map(|s| parse_line_from_string(s));
+    let all_points: Vec<Vec<Point>> = lines
+        .map(|line| generate_coordinates_from_line(line, "part_1"))
+        .collect();
     let mut map: HashMap<Point, i32> = HashMap::new();
-    for line in lines {
-        mark_on_map(line, &mut map)
+    for points in all_points {
+        mark_on_map(points, &mut map)
     }
-    // println!("map {:?}", map);
+    // pretty_print_map(&map);
     let dangerous_points: HashMap<&Point, &i32> = map.iter().filter(|(k, v)| v > &&1).collect();
     dangerous_points.len() as i32
 }
 
-fn mark_on_map((point_a, point_b): Line, mut map: &mut HashMap<Point, i32>) {
-    let points: Vec<Point> = generate_coordinates_from_line((point_a, point_b));
+fn solve_part_two(input: &str) -> i32 {
+    let lines = input.lines().into_iter().map(|s| parse_line_from_string(s));
+    let mut map: HashMap<Point, i32> = HashMap::new();
+    let all_points: Vec<Vec<Point>> = lines
+        .map(|line| generate_coordinates_from_line(line, "part_two"))
+        .collect();
+    for points in all_points {
+        mark_on_map(points, &mut map)
+    }
+    // pretty_print_map(&map);
+    let dangerous_points: HashMap<&Point, &i32> = map.iter().filter(|(k, v)| v > &&1).collect();
+    dangerous_points.len() as i32
+}
+
+fn mark_on_map(points: Vec<Point>, mut map: &mut HashMap<Point, i32>) {
     for point in points {
         match map.get(&point) {
             Some(val) => {
@@ -58,9 +72,30 @@ fn mark_on_map((point_a, point_b): Line, mut map: &mut HashMap<Point, i32>) {
             }
         }
     }
-    // println!("map {:?}", map);
 }
 
+fn pretty_print_map(map: &HashMap<Point, i32>) {
+    let max_x = map.iter().map(|(k, v)| k.y).max().unwrap();
+    let max_y = map.iter().map(|(k, v)| k.x).max().unwrap();
+    println!("max_x {}, max_y {}", &max_x, &max_y);
+    for y in 0..max_y + 1 {
+        print!("{}|", y);
+        for x in 0..max_x + 1 {
+            let point = map.get(&Point { x, y });
+            match point {
+                Some(val) => {
+                    print!("{}", val)
+                }
+                None => {
+                    print!(".")
+                }
+            }
+            if x == max_x {
+                print!("\n")
+            }
+        }
+    }
+}
 fn parse_line_from_string(s: &str) -> Line {
     let split: Vec<&str> = s.split_whitespace().collect();
     let first_part: Vec<&str> = split[0].split(',').collect();
@@ -78,15 +113,13 @@ fn parse_line_from_string(s: &str) -> Line {
     (point_a, point_b)
 }
 
-fn generate_coordinates_from_line((point_a, point_b): Line) -> Vec<Point> {
-    let mut direction = "";
+fn generate_coordinates_from_line((point_a, point_b): Line, part: &str) -> Vec<Point> {
+    let mut direction;
     if point_a.x == point_b.x {
         direction = "vertical";
-    }
-    if point_a.y == point_b.y {
+    } else if point_a.y == point_b.y {
         direction = "horizontal";
-    }
-    if direction == "" {
+    } else {
         direction = "diagonal";
     }
 
@@ -101,12 +134,6 @@ fn generate_coordinates_from_line((point_a, point_b): Line) -> Vec<Point> {
         (point_b.y, point_a.y)
     };
 
-    // println!("{:?}", direction);
-    // println!("{:?}", min_x);
-    // println!("{:?}", max_x);
-    // println!("{:?}", min_y);
-    // println!("{:?}", max_y);
-
     let mut covered_xs: Vec<i32> = vec![];
     let mut covered_ys: Vec<i32> = vec![];
     if direction == "horizontal" {
@@ -115,9 +142,18 @@ fn generate_coordinates_from_line((point_a, point_b): Line) -> Vec<Point> {
     } else if direction == "vertical" {
         covered_ys = (min_y..max_y + 1).collect();
         covered_xs = std::iter::repeat(min_x).take(covered_ys.len()).collect();
+    } else if part == "part_two" && direction == "diagonal" {
+        covered_xs = (min_x..max_x + 1).collect();
+        covered_ys = (min_y..max_y + 1).collect();
+        if point_a.x < point_b.x {
+            // when going '\' the x's need to descend
+            covered_xs.reverse()
+        }
+        if point_a.y < point_b.y {
+            // when going '/' the y's need to descend
+            covered_ys.reverse()
+        }
     }
-    // println!("{:?}", covered_xs);
-    // println!("{:?}", covered_ys);
     std::iter::zip(&covered_xs, &covered_ys)
         .into_iter()
         .map(|(x, y)| Point { x: *x, y: *y })
@@ -142,8 +178,8 @@ mod tests {
     fn test_1() {
         assert_eq!(solve_part_one(EXAMPLE_INPUT), 5);
     }
-    // #[test]
-    // fn test_2() {
-    //     assert_eq!(solve_part_two(EXAMPLE_INPUT), 1924);
-    // }
+    #[test]
+    fn test_2() {
+        assert_eq!(solve_part_two(EXAMPLE_INPUT), 12);
+    }
 }
