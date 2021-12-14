6f063e315e642f452b179e01db9ed13afccac8ee
Niels Renard <code.i.pushed@gmail.com>
2021-12-14 23:15:48 +0100

    2021/14 **

--- advent-of-rust-code/2021/day14/src/main.rs
+++ advent-of-rust-code/2021/day14/src/main.rs
@@ -1,264 +1,157 @@
 #![allow(dead_code, unused_mut, unused_variables, unused_imports)]
-#![feature(iter_advance_by, iter_zip, destructuring_assignment)]
-
+#![feature(iter_advance_by, iter_zip, destructuring_assignment, map_try_insert)]
+use itertools::Itertools;
 use std::collections::HashMap;
 #[macro_use]
 extern crate measure_time;
 
 fn input_from_file() -> String {
-    std::fs::read_to_string("../../data/2021/13.input").unwrap()
+    std::fs::read_to_string("../../data/2021/14.input").unwrap()
 }
 
 fn main() {
     print_time!("execution");
-    let answer_1 = solve_part_one(&input_from_file());
+    let answer_1 = solve_part_one(&input_from_file(), 10);
     println!("part 1: {}", answer_1);
-    let answer_2 = solve_part_two(&input_from_file());
+    let answer_2 = solve_part_two(&input_from_file(), 40);
+    println!("part 2: {}", answer_2);
+    //execution took 2ms
 }
 
-#[derive(Debug)]
-struct Cave {
-    connected_caves: Vec<usize>,
-    small: bool,
-}
+fn solve_part_one(input: &str, steps: usize) -> usize {
+    let (template, rules) = parse(input);
 
-fn solve_part_one(input: &str) -> u32 {
-    let (grid, fold_instructions): (Vec<Vec<u32>>, Vec<(char, usize)>) = parse(input);
-    // pretty_print(&grid);
-    let mut folded_grid = grid.clone();
-    for (direction, fold_line) in fold_instructions.iter().nth(0) {
-        match direction {
-            'x' => {
-                folded_grid = hacky_fold_left(&folded_grid, *fold_line);
-                // println!();
-                // println!("After fold_left");
-                // pretty_print(&folded_grid);
-            }
-            'y' => {
-                folded_grid = fold_up(&folded_grid, *fold_line);
-                // println!();
-                // println!("After fold_up");
-                // pretty_print(&folded_grid);
-            }
-            _ => panic!(),
-        }
+    let mut polymer = template;
+    for n in 1..=steps {
+        polymer = step(&polymer, &rules);
     }
-    folded_grid.iter().flatten().filter(|n| **n > 0).sum()
+    let counts: Vec<usize> = polymer
+        .chars()
+        .counts()
+        .into_values()
+        .sorted()
+        .collect_vec();
+    // println!("{:?}", polymer);
+    counts.last().unwrap() - counts[0]
 }
 
-fn solve_part_two(input: &str) -> u32 {
-    let (grid, fold_instructions): (Vec<Vec<u32>>, Vec<(char, usize)>) = parse(input);
-    // println!("Starting input");
-    // pretty_print(&grid);
-    let mut folded_grid = grid.clone();
-    for (direction, fold_line) in fold_instructions {
-        match direction {
-            'x' => {
-                folded_grid = fold_left_using_rotations(&folded_grid, fold_line);
-                // println!();
-                // println!("After fold_left along x={}", fold_line);
-                // pretty_print(&folded_grid);
-            }
-            'y' => {
-                folded_grid = fold_up(&folded_grid, fold_line);
-                // println!();
-                // println!("After fold_up along y={}", fold_line);
-                // pretty_print(&folded_grid);
-            }
-            _ => panic!(),
-        }
+fn solve_part_two(input: &str, steps: usize) -> usize {
+    let (template, rules) = parse(input);
+    let polymer: Vec<char> = template.chars().into_iter().collect::<Vec<_>>();
+    let mut initial_pairs: HashMap<String, usize> = HashMap::new();
+    polymer.windows(2).for_each(|pair| {
+        let key = format!("{}{}", pair[0], pair[1]);
+        *initial_pairs.entry(key).or_insert(0) += 1;
+    });
+
+    // println!("initial pairs: {:?}",  initial_pairs);
+    for n in 1..=steps {
+        step_pairs(&mut initial_pairs, &rules);
     }
-    pretty_print(&folded_grid);
-    folded_grid.iter().flatten().filter(|n| **n > 0).sum()
+
+    let mut letters: HashMap<char, usize> = HashMap::new();
+    let last_letter: &char = polymer.last().unwrap();
+    letters.insert(*last_letter, 1);
+    for (pair, count) in &initial_pairs {
+        let first_letter = pair.chars().into_iter().next().unwrap();
+        *letters.entry(first_letter).or_insert(0) += count;
+    }
+
+    // println!("Letters: {:?}",  letters);
+    let counts = letters.into_values().sorted().collect_vec();
+    counts.last().unwrap() - counts[0]
 }
 
-fn fold_up(grid: &Vec<Vec<u32>>, y_line: usize) -> Vec<Vec<u32>> {
-    let (above_ref, below_ref) = grid.split_at(y_line);
-    let (mut above, mut below): (Vec<Vec<u32>>, Vec<Vec<u32>>) =
-        (above_ref.to_vec().clone(), below_ref.to_vec().clone());
-    let without_fold_line = Vec::from_iter(below[1..below.len()].iter().cloned());
-    below = without_fold_line;
-    below.reverse();
+fn step_pairs(mut pair_counts_map: &mut HashMap<String, usize>, rules: &HashMap<String, String>) {
+    let pair_counts_list = Vec::from_iter(pair_counts_map.iter().map(|(k, v)| (k.clone(), *v)));
 
-    let max_x = grid[0].len();
-    let mut new_grid: Vec<Vec<u32>> = vec![vec![0; max_x]; below.len()];
-    for y in 0..above.len() {
-        for x in 0..max_x {
-            if above[y][x] == 1 || below[y][x] == 1 {
-                new_grid[y][x] = 1
-            }
+    for (pair, count) in pair_counts_list {
+        let new_letter = rules.get(&pair).unwrap();
+        // this unwrap() assumes there are rules for all combinations
+
+        let new_pair_a = format!("{}{}", pair.chars().into_iter().next().unwrap(), new_letter);
+        let new_pair_b = format!("{}{}", new_letter, pair.chars().into_iter().nth(1).unwrap());
+
+        // Subtract the corrent amount of broken connections
+        // Remove entry if there are none left
+        // Maybe this can be done in a smarter way
+        let current_pair = pair_counts_map.get_mut(&pair).unwrap();
+        if *current_pair > count {
+            *current_pair -= count
+        } else {
+            pair_counts_map.remove(&pair);
         }
+        // Insert newly created pairs
+        (*pair_counts_map.entry(new_pair_a).or_insert(0)) += count;
+        (*pair_counts_map.entry(new_pair_b).or_insert(0)) += count;
     }
-    new_grid
 }
 
-fn hacky_fold_left(grid: &Vec<Vec<u32>>, x_line: usize) -> Vec<Vec<u32>> {
-    let left_halves: Vec<Vec<u32>> = grid
-        .iter()
-        .map(|row| Vec::from_iter(row[0..x_line].iter().cloned()))
-        .collect();
-
-    let right_halves: Vec<_> = grid
-        .iter()
-        .map(|row| {
-            let max_x = row.len();
-            let mut right_half = Vec::from_iter(row[x_line + 1..max_x].iter().cloned());
-            right_half.reverse();
-            right_half
-        })
-        .collect();
-    let max_x = left_halves[0].len();
-    let max_y = grid.len();
-    let mut new_grid: Vec<Vec<u32>> = vec![vec![0; max_x]; max_y];
-    for y in 0..grid.len() {
-        for x in 0..left_halves[0].len() {
-            if left_halves[y][x] == 1 || right_halves[y][x] == 1 {
-                new_grid[y][x] = 1
-            }
-        }
-    }
-    new_grid
-}
-
-fn fold_left_using_rotations(grid: &Vec<Vec<u32>>, x_line: usize) -> Vec<Vec<u32>> {
-    /*
-    to rotate 90 degrees:
-     1. transpose array
-     2. reverse each row
-    */
-    // 1. transpose array (rows become columns)
-    let max_x = grid[0].len();
-    let max_y = grid.len();
-    let mut transpose: Vec<Vec<u32>> = vec![vec![0; max_y]; max_x];
-    for y in 0..max_y {
-        for x in 0..max_x {
-            transpose[x][y] = grid[y][x];
-        }
-    }
-    // 2. reverse each row
-    transpose.iter_mut().for_each(|r| r.reverse());
-
-    let mut rotated = fold_up(&transpose, x_line);
-
-    /*
-    to rotate -90 degrees:
-     1. transpose array
-     2. reverse each column
-    */
-    // 1. transpose array (rows become columns)
-    let mut rotated_back: Vec<Vec<u32>> = vec![vec![0; rotated.len()]; rotated[0].len()];
-    for y in 0..rotated.len() {
-        for x in 0..rotated[0].len() {
-            rotated_back[x][y] = rotated[y][x];
-        }
-    }
-    // 2. reverse each column
-    rotated_back.reverse();
-    rotated_back
-}
-
-fn parse(input: &str) -> (Vec<Vec<u32>>, Vec<(char, usize)>) {
-    let dots: Vec<(usize, usize)> = input
-        .lines()
+fn step(polymer: &str, rules: &HashMap<String, String>) -> String {
+    let mut new_polymer = String::new();
+    new_polymer.push(polymer.chars().next().unwrap());
+    polymer
+        .chars()
         .into_iter()
-        .take_while(|line| line.ne(&""))
-        .map(|dot_line| dot_line.split_once(',').unwrap())
-        .map(|(x, y)| (x.parse().unwrap(), y.parse().unwrap()))
-        .collect();
-    let folds: Vec<(char, usize)> = input
-        .lines()
-        .into_iter()
-        .skip_while(|line| line.ne(&""))
+        .collect::<Vec<_>>()
+        .windows(2)
+        .for_each(|pair: &[char]| {
+            let maybe_rule = rules.get(&*pair.iter().collect::<String>());
+            match maybe_rule {
+                Some(letter) => {
+                    new_polymer.push(letter.chars().next().unwrap());
+                    new_polymer.push(pair[1]);
+                }
+                None => {
+                    new_polymer.push(pair[1]);
+                }
+            }
+        });
+    new_polymer
+}
+
+fn parse(input: &str) -> (String, HashMap<String, String>) {
+    let mut lines_iter = input.lines().into_iter().peekable();
+    let template = lines_iter.next().unwrap().to_string();
+    let rules = lines_iter
         .skip(1)
-        .map(|line| line.chars().into_iter().skip(11).collect::<String>())
-        .map(|line| {
-            (
-                line.chars().next().unwrap(),
-                line.chars().skip(2).collect::<String>(),
-            )
-        })
-        .map(|(x, n)| (x, n.parse().unwrap()))
-        .collect();
+        .map(|line| line.split_once(" -> ").unwrap())
+        .map(|(k, v)| (k.to_string(), v.to_string()))
+        .collect::<HashMap<String, String>>();
 
-    // this is what was causing my bug!!! I assumed the highest x and y were the highest dots, ugh!
-    // let max_x = dots.iter().max_by_key(|(x,_)| x).unwrap().0 as usize;
-    // let max_y = dots.iter().max_by_key(|(_,y)| y).unwrap().1 as usize;
-
-    // making my output look like this:
-    
-    // ..##.##.#.####.####.#....####.#.##.#..#.
-    // ...#.####..##..####.#....#.##.###..####.
-    // #..#.###..##...#..#.#....#.##.#.#..#..#.
-    // ##.#.#.##.####.###..####.####.#.#..#..#.
-
-    // determine max x and y based on folds
-    let max_x = if folds[0].0 == 'x' {
-        folds[0].1 * 2 + 1
-    } else {
-        folds[1].1 * 2 + 1
-    };
-    let max_y = if folds[0].0 == 'y' {
-        folds[0].1 * 2 + 1
-    } else {
-        folds[1].1 * 2 + 1
-    };
-
-
-    
-    let mut grid: Vec<Vec<u32>> = vec![vec![0; max_x]; max_y];
-    dots.iter().for_each(|(x, y)| grid[*y][*x] = 1);
-    (grid, folds)
+    (template, rules)
 }
 
-fn pretty_print(grid: &Vec<Vec<u32>>) {
-    println!("x:{}, y:{}", grid[0].len(), grid.len());
-    let max_width = grid[0].len();
-    for r in grid.iter() {
-        for (i, n) in r.iter().enumerate() {
-            if *n == 0 {
-                print!(" ")
-            } else {
-                print!("#")
-            }
-            if i == max_width - 1 {
-                println!();
-            }
-        }
-    }
-}
 #[cfg(test)]
 mod tests {
 
     use super::*;
-    const EXAMPLE_INPUT: &str = "6,10
-0,14
-9,10
-0,3
-10,4
-4,11
-6,0
-6,12
-4,1
-0,13
-10,12
-3,4
-3,0
-8,4
-1,10
-2,14
-8,10
-9,0
+    const EXAMPLE_INPUT: &str = "NNCB
 
-fold along y=7
-fold along x=5";
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C";
 
     #[test]
     fn test_1() {
-        assert_eq!(solve_part_one(EXAMPLE_INPUT), 17);
+        assert_eq!(solve_part_one(EXAMPLE_INPUT, 10), 1588);
     }
 
     #[test]
     fn test_2() {
-        assert_eq!(solve_part_two(EXAMPLE_INPUT), 999);
+        assert_eq!(solve_part_two(EXAMPLE_INPUT, 40), 2188189693529);
     }
 }
